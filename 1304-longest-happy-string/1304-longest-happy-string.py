5a66ed7a82a3aa74f570af66c11a0ceb1c86ca49
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-03 17:21:17 -0700

    Time: 37 ms (46.03%), Space: 16.5 MB (47.88%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -8,23 +8,23 @@ class Solution:
         if c > 0:
             heapq.heappush(heap, [-c, "c"])
         
-        res = []
+        res = ""
         while heap:
-            first_count, first_char = heapq.heappop(heap)
-            if len(res) >= 2 and res[-1] == res[-2] == first_char:
+            tmp = heapq.heappop(heap)
+            if len(res) >= 2 and res[-1] == res[-2] == tmp[1]:
                 if not heap:
                     break
-                second_count, second_char = heapq.heappop(heap)
-                res.append(second_char)
-                second_count += 1
-                if second_count < 0:
-                    heapq.heappush(heap, [second_count, second_char])
-                heapq.heappush(heap, [first_count, first_char])
+                a = heapq.heappop(heap)
+                res += a[1]
+                a[0] += 1
+                if a[0] < 0:
+                    heapq.heappush(heap, a)
+                heapq.heappush(heap, tmp)
             else:
-                res.append(first_char)
-                first_count += 1
-                if first_count < 0:
-                    heapq.heappush(heap, [first_count, first_char])
+                res += tmp[1]
+                tmp[0] += 1
+                if tmp[0] < 0:
+                    heapq.heappush(heap, tmp)
         
-        return "".join(res)
+        return res
 
