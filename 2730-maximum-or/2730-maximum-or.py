1736883c9d9e6ced2a1d069a600a63c409b3cb86
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-03 21:51:18 -0700

    Time: 576 ms (54.67%), Space: 30.8 MB (85.33%) - LeetHub

--- 2730-maximum-or/2730-maximum-or.py
+++ 2730-maximum-or/2730-maximum-or.py
@@ -1,11 +1,10 @@
 class Solution:
     def maximumOr(self, nums: List[int], k: int) -> int:
         sur_fix = [0] * (len(nums)+1)
-        res = 0
         for i in range(len(nums)-1, -1, -1):
             sur_fix[i] = (nums[i] | sur_fix[i+1])
         
-        pre_fix = 0
+        pre_fix,res = 0, 0
         for i in range(len(nums)):
             tmp = pre_fix|(nums[i]*pow(2,k))|sur_fix[i+1]
             res = max(res, tmp)
