0938d775a011f298f67db95e7f71dfe8ccc96054
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-07 00:27:42 -0700

    Time: 1366 ms (8.23%), Space: 86.1 MB (5.27%) - LeetHub

---
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        restricted = set(restricted)
+        restricted.add(0)
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        res = [0]
+        def dfs(node):
+            res[0] += 1
+            print(node)
+            for i in graph[node]:
+                if i not in restricted:
+                    restricted.add(i)
+                    dfs(i)
+        dfs(0)
+        return res[0] 
