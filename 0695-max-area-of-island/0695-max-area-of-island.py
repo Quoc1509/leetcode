68e8afc2e7a3671c30894a97c054226436922f4e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-10 14:04:54 -0700

    Time: 103 ms (77.15%), Space: 17.2 MB (52.87%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,28 +1,40 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        surround = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        # visited = set()
+        # surround = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-        def bfs(row, col):
-            land = deque()
-            land.append((row, col))
+        # def bfs(row, col):
+        #     land = deque()
+        #     land.append((row, col))
+        #     grid[row][col] = 0
+        #     count = 1 
+        #     while land:
+        #         for _ in range(len(land)):
+        #             ro, co = land.popleft()
+        #             for a, b in surround:
+        #                 r, c = ro + a, co + b
+        #                 if 0 <= r < len(grid) and 0 <= c < len(grid[r]) and grid[r][c] == 1:
+        #                     land.append((r, c))
+        #                     grid[r][c] = 0
+        #                     count += 1
+        #     return count
+        # res = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[i])):
+        #         if grid[i][j] == 1:
+        #             temp = bfs(i, j)
+        #             res = max(res, temp)
+
+        def dfs(row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[row]) or grid[row][col] == 0:
+                return 0
             grid[row][col] = 0
-            count = 1 
-            while land:
-                for _ in range(len(land)):
-                    ro, co = land.popleft()
-                    for a, b in surround:
-                        r, c = ro + a, co + b
-                        if 0 <= r < len(grid) and 0 <= c < len(grid[r]) and grid[r][c] == 1:
-                            land.append((r, c))
-                            grid[r][c] = 0
-                            count += 1
-            return count
+            return 1 + dfs(row+1, col) + dfs(row-1, col) + dfs(row, col+1) + dfs(row, col -1)
         res = 0
         for i in range(len(grid)):
             for j in range(len(grid[i])):
                 if grid[i][j] == 1:
-                    temp = bfs(i, j)
+                    temp = dfs(i, j)
                     res = max(res, temp)
-        # print(grid)
+
+
         return res
