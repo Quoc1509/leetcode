30ba509cabdc6f015a1c310337c181f2ca7e1e7d
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-15 00:16:40 -0700

    Time: 602 ms (24.86%), Space: 22.8 MB (32.61%) - LeetHub

---
+++ 0922-possible-bipartition/0922-possible-bipartition.py
@@ -0,0 +1,23 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        for a, b in dislikes:
+            graph[a].append(b)
+            graph[b].append(a)
+        print(graph)
+        q = deque()
+        group = [set(), set()]
+        for key, item in graph.items():
+            if key not in group[0] and key not in group[1]:
+                q.append((key, 0))
+                group[0].add(key)
+                while q:
+                    for _ in range(len(q)):
+                        parents, num = q.popleft()
+                        temp = 0 if num == 1 else 1
+                        for c in graph[parents]:
+                            if c in group[num]: return False
+                            if c not in group[0] and c not in group[1]: q.append((c, temp))
+                            group[temp].add(c)
+                        
+        return True
