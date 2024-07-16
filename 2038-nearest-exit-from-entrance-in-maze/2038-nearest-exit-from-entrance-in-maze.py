5ad859d218316de68697379a11b83aada09a73d4
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-15 20:35:03 -0700

    Time: 657 ms (58.56%), Space: 17.2 MB (57.26%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,10 +1,10 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         surround = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-        q, visited = deque(), set()
-
+        q = deque()
+        maze[entrance[0]][entrance[1]] = '+'
         entrance = tuple(entrance)
-        visited.add(entrance)
+        # visited.add(entrance)
         q.append(entrance)
         d = 0
         while q:
@@ -12,10 +12,10 @@ class Solution:
                 row, col = q.popleft()
                 for x, y in surround:
                     r, c = row + x, col + y
-                    if 0 <= r < len(maze) and 0 <= c < len(maze[r]) and (r, c) not in visited and maze[r][c] == '.':
+                    if 0 <= r < len(maze) and 0 <= c < len(maze[r]) and maze[r][c] == '.':
                         if r == 0 or r == len(maze)-1 or c == 0 or c == len(maze[r])-1:
                             return d+1
                         q.append((r, c))
-                        visited.add((r, c))
+                        maze[r][c] = '+'
             d += 1
         return -1
