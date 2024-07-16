fa7a9819e22cdcb536555dd87da0bc1d9e947e96
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-15 20:32:38 -0700

    Time: 683 ms (33.77%), Space: 18.3 MB (13.4%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        surround = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        q, visited = deque(), set()
+
+        entrance = tuple(entrance)
+        visited.add(entrance)
+        q.append(entrance)
+        d = 0
+        while q:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                for x, y in surround:
+                    r, c = row + x, col + y
+                    if 0 <= r < len(maze) and 0 <= c < len(maze[r]) and (r, c) not in visited and maze[r][c] == '.':
+                        if r == 0 or r == len(maze)-1 or c == 0 or c == len(maze[r])-1:
+                            return d+1
+                        q.append((r, c))
+                        visited.add((r, c))
+            d += 1
+        return -1
