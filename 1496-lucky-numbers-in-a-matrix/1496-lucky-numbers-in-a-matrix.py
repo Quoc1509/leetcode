ccb35a2485e0af34266121e425087cd850f2abb9
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-18 17:55:22 -0700

    Time: 119 ms (25.75%), Space: 16.7 MB (79.04%) - LeetHub

---
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        maxArray = [-inf] * len(matrix[0])
+        minArray = [inf] * len(matrix)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                maxArray[j] = max(matrix[i][j], maxArray[j])
+            minArray[i] = min(matrix[i])
+        minArray = set(minArray)
+        res= []
+        for i in maxArray:
+            if i in minArray:
+                res.append(i)
+        return res
+        
