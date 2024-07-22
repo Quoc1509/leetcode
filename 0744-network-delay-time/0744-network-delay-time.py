36892f4e1704be1a66a76cb22f6e1a0dd7a19940
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-07-21 23:18:17 -0700

    Time: 351 ms (95.87%), Space: 18.6 MB (27.8%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -4,20 +4,15 @@ class Solution:
         l = [inf] * n
         for x,y,z in times:
             graph[x].append((z, y))
-        # print(graph)
         q = []
         heapq.heappush(q, (0, k))
         l[k-1] = 0
         while q:
             we, node = heapq.heappop(q)
             if we > l[node-1]: continue
-            # print(we, node)
             for w, ne in graph[node]:
                 if we + w < l[ne-1]:
                     heapq.heappush(q, (we+w, ne))
                     l[ne-1] = we+w
-        res = 0
-        for i in l:
-            if i == inf: return -1
-            res = max(res, i)
-        return res
+        res = max(l)
+        return -1 if res == inf else res
