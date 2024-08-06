b960bb89e1196e3e30fb6f84012f08e83a40cb34
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-05 17:20:13 -0700

    Time: 497 ms (42.13%), Space: 31.2 MB (83.15%) - LeetHub

---
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        l, res = 0, 0
+        total = 0
+
+        for r in range(len(nums)):
+            while (total & nums[r]) != 0:
+                
+                total ^= nums[l]
+                l += 1
+           
+            total |= nums[r]
+            res = max(res, r - l + 1)
+
+        return res
