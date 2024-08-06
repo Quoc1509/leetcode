345ebf76d8572787d4cf597b7cf1115e2af5fd2e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-05 20:18:35 -0700

    Time: 794 ms (56.21%), Space: 29.2 MB (50.3%) - LeetHub

--- 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
+++ 1985-maximum-subarray-min-product/1985-maximum-subarray-min-product.py
@@ -16,31 +16,8 @@ class Solution:
                 j = stack.pop()
                 l = stack[-1] if stack else -1
                 r = i
-                res = max(res, (prefix_sum[r] - prefix_sum[l])*nums[i])
+                res = max(res, (prefix_sum[r] - prefix_sum[l+1])*nums[j])
             stack.append(i)
         return res % MOD
 
 
-class Solution:
-    def maxSumMinProduct(self, nums: List[int]) -> int:
-        MOD = 10**9 + 7
-        n = len(nums)
-        res = 0
-        
-        # Compute prefix sums
-        prefix_sum = [0] * (n + 1)
-        for i in range(n):
-            prefix_sum[i + 1] = prefix_sum[i] + nums[i]
-        
-        stack = []
-        for i in range(n+1):
-            minNum = 0 if i == n else nums[i]
-            while stack and nums[stack[-1]] > minNum:
-                j = stack.pop()
-                l = stack[-1] if stack else -1
-                r = i
-                # Calculate the maximum sum of the minimum product
-                res = max(res, nums[j] * (prefix_sum[r] - prefix_sum[l+1]))
-            stack.append(i)
-        
-        return res % MOD
