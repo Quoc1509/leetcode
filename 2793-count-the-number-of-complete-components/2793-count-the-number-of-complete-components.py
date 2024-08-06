0e51d37a077b718085c2ab2fd7d59452d4e6e621
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-05 17:19:38 -0700

    Time: 562 ms (37.12%), Space: 17 MB (84.89%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        visited = set()
+
+        def bfs(node):
+            tmp = set()
+            count = 1
+            q = deque([node])
+            visited.add(node)
+            while q:
+                for _ in range(len(q)):
+                    n = q.popleft()
+                    tmp.add(len(graph[n]))
+                    for j in graph[n]:
+                        if j not in visited:
+                            visited.add(j)
+                            count += 1
+                            q.append(j)
+            return (tmp, count)
+        res = 0
+        for i in range(n):
+            if i not in visited:
+                a, c = bfs(i)
+                b = list(a)
+                print(len(b), b)
+                if len(b) == 1 and b[0] == c-1:
+                    res += 1
+        return res
+        
