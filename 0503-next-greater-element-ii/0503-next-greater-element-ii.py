5788e71fae584934253b7d7de346de2a35bdb7eb
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-07 14:47:08 -0700

    Time: 152 ms (55.96%), Space: 18.2 MB (88.03%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         res = [-1] * len(nums)
+        length = len(nums)
         stack = []
         nums = nums + nums
         for i in range(len(nums)):
@@ -8,7 +9,7 @@ class Solution:
                 stack.append(i)
                 continue
             while stack and nums[stack[-1]] < nums[i]:
-                res[stack[-1]%len(res)] = nums[i]
+                res[stack[-1]%length] = nums[i]
                 stack.pop()
             stack.append(i)     
            
