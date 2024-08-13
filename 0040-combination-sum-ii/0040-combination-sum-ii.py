569dbb23768f65f65435d2a4b75209955d3c80dd
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-13 12:31:41 -0700

    Time: 65 ms (46.82%), Space: 16.6 MB (30.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        def backTracking(total, index, memo):
+        def backTracking(total, index):
             
             if total == target: return [[]]
             if total > target: return None
@@ -10,7 +10,7 @@ class Solution:
             for i in range(index, len(candidates)):
                 if i > index and candidates[i] == candidates[i - 1]:
                     continue
-                temp =  backTracking(total+candidates[i], i+1, memo)
+                temp =  backTracking(total+candidates[i], i+1)
 
                 if temp is not None:
                     for j in temp:
@@ -19,5 +19,5 @@ class Solution:
                         res.append(cur)
                     
             return res
-        return backTracking(0, 0, {})
+        return backTracking(0, 0)
 
