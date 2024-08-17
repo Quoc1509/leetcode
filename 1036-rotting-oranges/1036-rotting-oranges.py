c45467be137cff0082788226d7468d88f619f118
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-17 00:55:09 -0700

    Time: 53 ms (39.6%), Space: 16.6 MB (17.37%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,38 +1,38 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        rott = []
+        rott = deque([])
+        fresh = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1:
-                    grid[i][j] = inf
                 if grid[i][j] == 2:
                     rott.append((i, j))
-                    grid[i][j] = 0
-        
+                if grid[i][j] == 1:
+                    fresh += 1
+        if fresh == 0: return 0
         surround = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        count = 1
+        while rott:
+            for _ in range(len(rott)):
+                ro, co = rott.popleft()
+                for x, y in surround:
+                    row, col = ro + x, co + y
+                    if 0 <= row < m and 0 <= col < n and grid[row][col] == 1:
+                        grid[row][col] = 2
+                        rott.append((row, col))
+                        fresh -= 1
+                        if fresh == 0:
+                            return count
+            count += 1
+        return -1
 
-        def bfs(r, c):
-            q = deque()
-            q.append((r, c))
-            count = 1
-            while q:
-                for _ in range(len(q)):
-                    ro, co = q.popleft()
-                    for x, y in surround:
-                        row, col = ro + x, co + y
-                        if 0 <= row < m and 0 <= col < n and grid[row][col] != 0 and grid[row][col] > count:
-                            grid[row][col] = min(grid[row][col], count)
-                            q.append((row, col))
-                count += 1
 
-
-        for a, b in rott:
-            bfs(a, b)
-        res = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == inf:
-                    return -1
-                res = max(res, grid[i][j])
-        return res
+        # for a, b in rott:
+        #     bfs(a, b)
+        # res = 0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j] == inf:
+        #             return -1
+        #         res = max(res, grid[i][j])
+        # return res
