4a7ebf3e9e171820a4308df906fe485144c19d44
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-22 17:13:13 -0700

    Time: 41 ms (37.24%), Space: 16.4 MB (83.72%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        if n == 1: return 0
+        count = 0
+        while n > 2:
+            if n % 2 == 0:
+                n //= 2
+            else:
+                if n == 3: return count + 2
+                temp = (n + 1) // 2
+                
+                if temp % 2 == 0:
+                    n = n+1
+                else: n -= 1
+            count += 1
+        return count + 1
