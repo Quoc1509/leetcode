7e1d16a4df9f82b6a7b2334939aabd9ffc08ee33
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-23 18:43:07 -0700

    Time: 54 ms (50.78%), Space: 16.6 MB (41.52%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,12 +3,16 @@ class Solution:
         N = len(candidates)
         res = []
 
-        def backTracking(index, path, total):
-            if total > target: return
-            if total == target: res.append(path[:])
-
-            for i in range(index, N):
-                backTracking(i, path+[candidates[i]], total + candidates[i])
+        def backTracking(i, path, total):
+            if total == target: 
+                res.append(path[:])
+                return
+            if total > target or i == N: return
+            t = i+1
+            while t < N and candidates[t] == candidates[i]:
+                t += 1
+            backTracking(t, path, total)
+            backTracking(i, path+[candidates[i]], total + candidates[i])
         backTracking(0, [], 0)
         return res
         
