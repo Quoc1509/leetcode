7f6b38fd538f628e658134783943841d10880f39
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-25 00:00:31 -0700

    Time: 45 ms (13.3%), Space: 16.8 MB (47.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -8,8 +8,7 @@ class Solution:
                 return
             if o > n or c > n: return
             if o > c:
-                dfs(s+")", o, c+1)
-            
+                dfs(s+")", o, c+1)         
             dfs(s+"(", o+1, c)
         dfs('', 0, 0)
         return res
