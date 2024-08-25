d421127800f7eb207246b8a0009ccae847205a34
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-25 00:00:20 -0700

    Time: 36 ms (67.57%), Space: 16.9 MB (47.08%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -7,10 +7,9 @@ class Solution:
                 res.append(s)
                 return
             if o > n or c > n: return
-            if o == c:
-                dfs(s+"(", o+1, c)
-            else:
-                dfs(s+"(", o+1, c)
+            if o > c:
                 dfs(s+")", o, c+1)
+            
+            dfs(s+"(", o+1, c)
         dfs('', 0, 0)
         return res
