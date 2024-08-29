c0f6e405df62b6e9454a2ea51290f4044f442051
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-08-28 19:49:37 -0700

    Time: 147 ms (31.95%), Space: 19.7 MB (5.15%) - LeetHub

---
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        xgraph = defaultdict(list)
+        ygraph = defaultdict(list)
+        for a, b in stones:
+            xgraph[a].append((a, b))
+            ygraph[b].append((a, b))
+        
+        count = [-1]
+        visited = set()
+        def dfs(x, y):
+            count[0] += 1
+            visited.add((x, y))
+            for r, c in xgraph[x]:
+                if (r, c) not in visited:
+                    dfs(r, c)
+            for r, c in ygraph[y]:
+                if (r, c) not in visited:
+                    dfs(r, c)
+        res = 0
+        for a, b in stones:
+            count[0] = -1
+            dfs(a, b)
+            res += count[0]
+            
+            # res = max(res, count[0])
+        return res
