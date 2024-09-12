593804c3ba2bb6892244fb2dccff7c725cee107b
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-12 11:30:31 -0700

    Time: 35 ms (59.45%), Space: 16.9 MB (5.51%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        @cache
+        def dfs(i, j):
+            if i == m and j == n:
+                return 1
+            if i > m or j > n: return 0
+
+            return dfs(i+1, j) + dfs(i, j+1)
+        return dfs(1, 1)
