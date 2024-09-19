722c1626e0f319ee88938029cab1663602d4940c
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-18 18:02:51 -0700

    Time: 741 ms (26.35%), Space: 64.9 MB (14.05%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -6,7 +6,6 @@ class Solution:
             if len(path) == k:
                 res.append(path[:])
                 return
-            if i > n: return
             for j in range(i, n+1):
                 dfs(j+1, path+[j])
 
