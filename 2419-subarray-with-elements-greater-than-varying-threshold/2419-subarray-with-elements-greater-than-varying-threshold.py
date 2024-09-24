e8dcf5e1dfd35171cdc9621d6f4c81f1977f4fb2
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-23 18:07:40 -0700

    Time: 811 ms (53.6%), Space: 31.2 MB (57.09%) - LeetHub

---
+++ 2419-subarray-with-elements-greater-than-varying-threshold/2419-subarray-with-elements-greater-than-varying-threshold.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validSubarraySize(self, nums: List[int], threshold: int) -> int:
+        nums.append(-inf)
+        stack = []
+        res = -inf
+        for i in range(len(nums)):
+            while stack and nums[stack[-1]] >= nums[i]:
+                ind = stack.pop()
+                if  nums[ind] > threshold/(i-(stack[-1]+1 if stack else 0)):
+                    # return i-(stack[-1]+1 if stack else 0)
+                    res = max(res, i-(stack[-1]+1 if stack else 0))
+            stack.append(i)
+        
+        return res if res != -inf else -1
