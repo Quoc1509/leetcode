666766b30b38eea279ccda169daf7781515bf22e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-23 18:45:46 -0700

    Time: 81 ms (19.45%), Space: 21.2 MB (5.61%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -6,7 +6,6 @@ class Solution:
             diffMP[a] += 1
             diffMP[b] -= 1
         arr = sorted([[a, b] for a, b in diffMP.items()])
-        print(arr)
         res = arr[0][1]
         for i in range(1, len(arr)):
             arr[i][1] = arr[i-1][1] + arr[i][1]
