25f08cede1bc6f6d579c9442d26e7fb8d43b1a1c
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-23 18:45:35 -0700

    Time: 89 ms (5.45%), Space: 21.6 MB (5.61%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        length = 0
-        for a, b in intervals:
-            length = max(length, b)
-        diffArr = [0] * (length+1)
+        diffMP = defaultdict(int)
 
         for a, b in intervals:
-            diffArr[a] += 1
-            diffArr[b] -= 1
-        res = diffArr[0]
-        for i in range(1, len(diffArr)):
-            diffArr[i] = diffArr[i-1] + diffArr[i]
-            res = max(diffArr[i], res)
+            diffMP[a] += 1
+            diffMP[b] -= 1
+        arr = sorted([[a, b] for a, b in diffMP.items()])
+        print(arr)
+        res = arr[0][1]
+        for i in range(1, len(arr)):
+            arr[i][1] = arr[i-1][1] + arr[i][1]
+            res = max(arr[i][1], res)
          
         return res
