5691fa40d7c0d5297dcd932b823c1957b540d5cc
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-23 19:41:26 -0700

    Time: 45 ms (27.45%), Space: 16.9 MB (12.56%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,15 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {}
-        def dfs(st):
-            if st == "": return True
-            if st in memo: return memo[st]
-            for i in wordDict:
-                l = len(i)
-                temp = st[:l]
-                if i == temp:
-                    if dfs(st[l:]):
+        words = set(wordDict)
+
+        @cache
+        def dfs(i):
+            if i == len(s):
+                return True
+            cur = ''
+            for j in range(i,len(s)):
+                cur += s[j]
+                if cur in words:
+                    if dfs(j+1):
                         return True
-            memo[st] = False
-            return memo[st]
-        return dfs(s) 
+            return False
+        return dfs(0)
