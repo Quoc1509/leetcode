74d5b04836a5f5ad9b9a5a5570cb05805b5eb422
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-25 01:37:53 -0700

    Time: 459 ms (67.75%), Space: 30.7 MB (8.53%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -3,22 +3,27 @@ class Solution:
         total = sum(matchsticks)
         if total % 4 != 0: return False
         slide = total // 4
-        @cache
+        memo = {}
+        # @cache
         def backTracking(length, count, visit):
             if count == 4: return True
             if length > slide: return False
+            if (length, count, visit) in memo: return memo[(length, count, visit)]
             for i in range(len(matchsticks)):
                 if (1<<i) & visit:
                     continue
                 length += matchsticks[i]
                 if length == slide:
                     if backTracking(0, count + 1, (1<<i)|visit):
-                        return True
+                        memo[(length, count, visit)] = True
+                        return memo[(length, count, visit)]
                 else:
                     if backTracking(length, count, (1<<i)|visit):
-                        return True
+                        memo[(length, count, visit)] = True
+                        return memo[(length, count, visit)]
                 length -= matchsticks[i]
-            return False
+            memo[(length, count, visit)] = False
+            return memo[(length, count, visit)]
         
         return backTracking(0,0,0)
 
