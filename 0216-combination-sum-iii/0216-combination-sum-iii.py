9281707fa426be47aad9676d72c39e4f5c10832b
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-25 23:13:01 -0700

    Time: 32 ms (78.5%), Space: 16.6 MB (35.35%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,15 +1,14 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        A = [1,2,3,4,5,6,7,8,9]
         res = []
 
-        def backTracking(total, i, r, path):
-            if total == n and r == 0:
+        def backTracking(total, index, path):
+            if total == n and len(path) == k:
                 res.append(path[:])
                 return
-            if total > n or r < 0 or i == len(A): return
-            backTracking(total, i+1, r, path)
-            backTracking(total + A[i], i+1, r-1, path+[A[i]])
+            if total > n : return
+            for i in range(index, 10):
+                backTracking(total + i, i +1, path+[i])
 
-        backTracking(0, 0, k, [])
+        backTracking(0, 1, [])
         return res         
