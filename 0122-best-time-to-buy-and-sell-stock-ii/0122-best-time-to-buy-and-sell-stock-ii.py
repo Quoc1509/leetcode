28e6774c5df1e9d58fc0fc1f25495d61b9ff04d8
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-09-27 18:43:00 -0700

    Time: 65 ms (16.67%), Space: 20.1 MB (5.26%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -9,17 +9,35 @@ class Solution:
         #     res += (i-mStock)
         #     mStock = i
         # return res
-        @cache
-        def dfs(i, holding):
-            if i == len(prices):
-                return 0
-            if holding:
-                sell = dfs(i+1, 0) + prices[i]
-                noSell = dfs(i+1, holding) 
-                return max(sell, noSell)
-            else:
-                buy = dfs(i+1, 1) -prices[i]
-                noBuy = dfs(i+1, 0)
-                return max(buy, noBuy)
 
-        return dfs(0, 0)
+        #top-down
+        # @cache
+        # def dfs(i, holding):
+        #     if i == len(prices):
+        #         return 0
+        #     if holding:
+        #         sell = dfs(i+1, 0) + prices[i]
+        #         noSell = dfs(i+1, holding) 
+        #         return max(sell, noSell)
+        #     else:
+        #         buy = dfs(i+1, 1) -prices[i]
+        #         noBuy = dfs(i+1, 0)
+        #         return max(buy, noBuy)
+
+        # return dfs(0, 0)
+
+        #bottom-up
+        N = len(prices)
+        dp = [[-inf] * 2 for _ in range(N+1)]
+        for i in range(2):
+            dp[N][i] = 0 
+
+        for i in range(N-1, -1, -1):
+            for holding in range(2):
+                if holding:
+                    dp[i][holding] = max(dp[i+1][0] + prices[i], dp[i+1][holding])
+                else:
+                    dp[i][holding] = max(dp[i+1][1] - prices[i], dp[i+1][holding])
+
+        return dp[0][0]
+                
