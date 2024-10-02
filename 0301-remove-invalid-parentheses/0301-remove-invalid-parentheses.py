7afd59dd8c500b5a273daf4c63cd49573b88f1f7
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-01 23:23:36 -0700

    Time: 53 ms (91.24%), Space: 16.7 MB (72.87%) - LeetHub

--- 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
+++ 0301-remove-invalid-parentheses/0301-remove-invalid-parentheses.py
@@ -14,7 +14,7 @@ class Solution:
         res = []
         def dfs(path, index, pair, k):
             if index == len(s): 
-                if pair == 0 and k == 0 and len(path) == len(s)-extra:
+                if pair == 0 and k == 0:
                     res.append(path[:])
                 return
             if k < 0 or pair < 0:
@@ -22,16 +22,12 @@ class Solution:
             if s[index] != '(' and s[index] != ')':
                 dfs(path + s[index], index+1, pair, k)
                 return
-
             j = index
             while j < len(s) and s[index] == s[j]:
                 j += 1
-
             for h in range(min(k, j-index) + 1):
                 a = j-index-h
                 npair = pair + (a if s[index] == '(' else -a)
                 dfs(path + s[index:index+a], j, npair, k-h)
-
-        dfs('', 0, 0, extra)
-        
+        dfs('', 0, 0, extra)      
         return res if res else [""]
