cc7e362b8c2c4ca98a87e583909b91a19f64f513
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-02 18:14:27 -0700

    Time: 32 ms (81.1%), Space: 16.6 MB (9%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -2,16 +2,29 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
         if N == 1: return nums[0]
-        memo = {}
-        def backTracking(i, head):
-            if head:
-                if i == N-1: return 0
-            if i >= N: return 0
-            if (i, head) in memo: return memo[i, head]
+        # memo = {}
+        # def backTracking(i, head):
+        #     if head:
+        #         if i == N-1: return 0
+        #     if i >= N: return 0
+        #     if (i, head) in memo: return memo[i, head]
 
-            res = max(backTracking(i+1, head), backTracking(i+2, head)+nums[i])
-            memo[i, head] = res
-            return memo[i, head]
-        return max(backTracking(0, True), backTracking(1, False))
+        #     res = max(backTracking(i+1, head), backTracking(i+2, head)+nums[i])
+        #     memo[i, head] = res
+        #     return memo[i, head]
+        # return max(backTracking(0, True), backTracking(1, False))
+
+        dp = [[0] * 2 for _ in range(N+2)]
+        
+
+        for i in range(N-1, -1, -1):
+            for head in range(2):
+                if head and i == N-1:
+                    continue
+                dp[i][head] = max(dp[i+1][head], dp[i+2][head]+nums[i])
+
+        return max(dp[0][1], dp[1][0])
+
+        
         
 
