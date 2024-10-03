bf38d382cfd7ec7347c5b36aef28b66c815af0cd
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-02 20:31:55 -0700

    Time: 2458 ms (27.15%), Space: 24.5 MB (10.95%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -3,14 +3,16 @@ class Solution:
         res = []
         N = len(s)
         dp = [[False]*N for _ in range(N)]            
-
+        left, right = 0, 1
         for l in range(1, N+1):
             for i in range(N-l+1):
                 j = i+l-1
 
                 if i == j or (i+1 == j and s[i] == s[j]) or (s[i] == s[j] and dp[i+1][j-1]):
                     dp[i][j] = True
-                    if (j-i+1) > len(res):
-                        res = s[i:j+1]
+                    if (j-i) > (left-right):
+                        left = i
+                        right = j
+                        # res = s[i:j+1]
 
-        return res
+        return s[left:right+1]
