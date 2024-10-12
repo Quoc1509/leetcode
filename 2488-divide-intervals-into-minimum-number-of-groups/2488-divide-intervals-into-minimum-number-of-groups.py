817bc6653992d7fbb781d31b2272a7fc8cce61aa
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-11 19:00:11 -0700

    Time: 1058 ms (81.43%), Space: 56.3 MB (64.35%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -1,13 +1,23 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        hashMap = defaultdict(int)
-        for a, b in intervals:
-            hashMap[a] += +1
-            hashMap[b+1] += -1
-        count = [[a, b] for a, b in hashMap.items()]
-        count.sort()
-        res = count[0][1]
-        for i in range(1, len(count)):
-            count[i][1] += count[i-1][1]
-            res = max(res, count[i][1])
-        return res
+        # hashMap = defaultdict(int)
+        # for a, b in intervals:
+        #     hashMap[a] += +1
+        #     hashMap[b+1] += -1
+        # count = [[a, b] for a, b in hashMap.items()]
+        # count.sort()
+        # res = count[0][1]
+        # for i in range(1, len(count)):
+        #     count[i][1] += count[i-1][1]
+        #     res = max(res, count[i][1])
+        # return res
+        intervals.sort()
+        heap = []
+        for start, end in intervals:
+            
+            if heap and heap[0] < start:
+                heappop(heap)
+            heappush(heap, end)
+
+            
+        return len(heap)
