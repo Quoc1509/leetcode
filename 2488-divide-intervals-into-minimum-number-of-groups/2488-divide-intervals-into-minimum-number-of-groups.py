b5102a3388d8318293e1593b3201375dd292c47e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-11 18:50:37 -0700

    Time: 1279 ms (21.31%), Space: 74.4 MB (5.06%) - LeetHub

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        hashMap = defaultdict(int)
+        for a, b in intervals:
+            hashMap[a] += +1
+            hashMap[b+1] += -1
+        count = [[a, b] for a, b in hashMap.items()]
+        count.sort()
+        res = count[0][1]
+        for i in range(1, len(count)):
+            count[i][1] += count[i-1][1]
+            res = max(res, count[i][1])
+        return res
