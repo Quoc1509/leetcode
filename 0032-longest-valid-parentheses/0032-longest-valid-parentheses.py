d21e19d64ee76b64fcfc3142334faf651674cb7b
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-14 16:09:40 -0700

    Time: 47 ms (31.58%), Space: 17.4 MB (21.2%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        temp = [0] * len(s)
+        stack = []
+        for i, e in enumerate(s):
+            if e == '(':
+                stack.append(i)
+            else:
+                if stack:
+                    index = stack.pop()
+                    temp[i] = 1
+                    temp[index] = 1
+        res = 0
+        count = 0
+        for i in range(len(temp)):
+            if temp[i] == 0:
+                count = 0
+            else:
+                count += 1
+                res = max(res, count)
+        return res
+
+
