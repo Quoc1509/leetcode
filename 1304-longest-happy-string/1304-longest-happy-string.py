31a5e694883ddeddf4d1491dac5769885b9587fc
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-15 17:24:27 -0700

    Time: 34 ms (69.04%), Space: 16.5 MB (94.09%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -2,29 +2,25 @@ class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
         heap = []
         if a > 0:
-            heapq.heappush(heap, [-a, "a"])
+            heappush(heap, [-a, 'a'])
         if b > 0:
-            heapq.heappush(heap, [-b, "b"])
+            heappush(heap, [-b, 'b'])
         if c > 0:
-            heapq.heappush(heap, [-c, "c"])
-        
-        res = ""
+            heappush(heap, [-c, 'c'])
+        res = ''
         while heap:
-            tmp = heapq.heappop(heap)
-            if len(res) >= 2 and res[-1] == res[-2] == tmp[1]:
+            cur = heappop(heap)
+            if len(res) > 1 and res[-1] == res[-2] == cur[1]:
                 if not heap:
                     break
-                a = heapq.heappop(heap)
-                res += a[1]
-                a[0] += 1
-                if a[0] < 0:
-                    heapq.heappush(heap, a)
-                # heapq.heappush(heap, tmp)
+                temp = heappop(heap)
+                res += temp[1]
+                temp[0] += 1
+                if temp[0] < 0:
+                    heappush(heap, temp)
             else:
-                res += tmp[1]
-                tmp[0] += 1
-            if tmp[0] < 0:
-                heapq.heappush(heap, tmp)
-        
+                res += cur[1]
+                cur[0] += 1
+            if cur[0] < 0:
+                heappush(heap, cur)
         return res
-
