8637b0514f34772f0ad4eafeb8cb4a29eb3ae596
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-21 00:12:56 -0700

    Time: 7 ms (97.85%), Space: 19 MB (98.24%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,14 +1,11 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        diffMP = defaultdict(int)
-
+        intervals.sort()
+        heap = []
         for a, b in intervals:
-            diffMP[a] += 1
-            diffMP[b] -= 1
-        arr = sorted([[a, b] for a, b in diffMP.items()])
-        res = arr[0][1]
-        for i in range(1, len(arr)):
-            arr[i][1] = arr[i-1][1] + arr[i][1]
-            res = max(arr[i][1], res)
-         
-        return res
+            temp = 0
+            if heap and heap[0] <= a:
+                temp = heappop(heap)
+            heappush(heap, max(b, temp))
+        # print(heap)
+        return len(heap)
