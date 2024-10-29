c9095a2ce93427f4bcd18840a4be1716f6a20591
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-28 20:08:23 -0700

    Time: 554 ms (6.06%), Space: 42.6 MB (5.3%) - LeetHub

---
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxMoves(self, grid: List[List[int]]) -> int:
+        M, N = len(grid), len(grid[0])
+        moves = [(-1,1), (0, 1), (1, 1)]
+        memo = {}
+        res = [0]
+        def dfs(r, c):
+            if (r,c) in memo: return memo[(r, c)]
+            count = -1
+            for x, y in moves:
+                ro, co = r+x, c+y
+                print(ro, co, r, c)
+                if 0 <= ro < M and 0 <= co < N and grid[r][c] < grid[ro][co]:
+                    count = max(count, dfs(ro, co))
+            memo[(r, c)] = count+1
+            res[0] = max(res[0], count + 1)
+            return memo[(r, c)]
+
+        for i in range(M):
+
+            dfs(i, 0)
+        return res[0]        
+                
+
