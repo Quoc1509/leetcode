d202519fb1f1cf93e98b63f62db37dcb18d61b6e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-29 23:31:46 -0700

    Time: 0 ms (100%), Space: 16.6 MB (11.16%) - LeetHub

--- 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.py
+++ 3334-apple-redistribution-into-boxes/3334-apple-redistribution-into-boxes.py
@@ -1,15 +1,10 @@
 class Solution:
     def minimumBoxes(self, apple: List[int], capacity: List[int]) -> int:
         capacity.sort(reverse=True)
-        l, r = 1, len(capacity)-1
-        apples = [sum(apple)]
-        def check(m):
-            total = sum(capacity[:m])
-            return total >= apples[0]
-        while l <= r:
-            mid = (l+r)//2
-            if check(mid):
-                r = mid - 1
-            else:
-                l = mid + 1
-        return l
+        apples = sum(apple)
+        total = 0
+        for i in range(len(capacity)):
+            total += capacity[i]
+            if total >= apples:
+                return i+1
+        return -1
