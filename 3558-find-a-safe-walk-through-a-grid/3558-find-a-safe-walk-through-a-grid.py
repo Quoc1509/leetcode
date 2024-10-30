bae6e82134563e9d8280ebb2b4daba489f5f24e0
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-29 19:00:54 -0700

    Time: 1660 ms (15.56%), Space: 68.5 MB (6.77%) - LeetHub

---
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        M, N = len(grid), len(grid[0])
+        visit = set()
+        surround = [(1,0),(0,1),(-1,0),(0,-1)]
+        @cache
+        def dfs(row, col, k):
+            if k == 0:
+                return False
+            if row == M-1 and col == N-1: return True
+            check = False
+            visit.add((row, col)) 
+            for x, y in surround:
+                r, c = row+x, col+y
+                if 0 <= r < M and 0 <= c < N and (r, c) not in visit:
+                    if grid[r][c] == 1 and k > 1:
+                        k -= 1
+                        if dfs(r, c, k):
+                            check = True
+                        k += 1
+                    elif grid[r][c] == 0:
+                        if dfs(r, c, k):
+                            check = True
+            visit.remove((row, col))
+            return check
+        if grid[0][0] == 1:
+            health -= 1
+        return dfs(0, 0, health)
+
+
+                    
