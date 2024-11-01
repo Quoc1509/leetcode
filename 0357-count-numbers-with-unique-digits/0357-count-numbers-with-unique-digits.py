c68f4eec4bc2a50553989a68783ac45c0fd31436
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-10-31 22:30:37 -0700

    Time: 6 ms (9.38%), Space: 17.5 MB (11.51%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        @cache
+        def dfs(visit, num):
+            print(num)
+            if num == n:
+                return 1
+            count = 0
+            for i in range(10):
+                if (1 << i) & visit or (num == 0 and i == 0):
+                    continue
+                count += dfs((1<<i)|visit, num+1)
+            return count+1
+        
+        return dfs(0, 0)
