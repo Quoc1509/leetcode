cca2e4aa02c7ad3ed8f775959975157112506f95
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-13 17:26:23 -0800

    Time: 491 ms (8.07%), Space: 79.7 MB (5.42%) - LeetHub

--- 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.py
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.py
@@ -6,43 +6,22 @@
 #         self.right = right
 class Solution:
     def treeQueries(self, root: Optional[TreeNode], queries: List[int]) -> List[int]:
-        dp = defaultdict(int)
-        dp[-1] = -1
-        index = defaultdict(list)
-        res = [0] * len(queries)
-        for i, e in enumerate(queries):
-            index[e].append(i)
-
+        res = []
+        @cache
         def dfs1(node):
-            l = r = 0
-            if node.left:
-                l = dfs1(node.left)
-            if node.right:
-                r = dfs1(node.right)
-            dp[node.val] = max(l, r)
+            if not node: return -1
+            l = dfs1(node.left)
+            r = dfs1(node.right)
             return max(l, r) + 1
 
-
-        def update(ind, numB):
-            for i in index[ind]:
-                res[i] = numB
-            del index[ind]
-
-
-
-        def dfs2(node, d, maxBranch): 
-            
+        dic = {}
+        def dfs2(node, d, maxBranch):       
             if not node: return
-            if node.left:
-                temp = max(maxBranch, d+1+dp[node.right.val if node.right else -1])
-                if node.left.val in index:
-                    update(node.left.val, temp)
-                dfs2(node.left, d+1, temp)
-            if node.right:
-                temp = max(maxBranch, d+1+dp[node.left.val if node.left else -1])
-                if node.right.val in index:
-                    update(node.right.val, temp)
-                dfs2(node.right, d+1, temp)
+            dic[node.val] = maxBranch  
+            dfs2(node.left, d+1, max(maxBranch, d+1+dfs1(node.right)))
+            dfs2(node.right, d+1, max(maxBranch, d+1+dfs1(node.left)))
         dfs1(root)
         dfs2(root, 0, 0)
+        for q in queries:
+            res.append(dic[q])
         return res
