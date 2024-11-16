8c5ecac11e2dd92de6116306613bfa47de13eacf
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-16 00:01:05 -0800

    Time: 5240 ms (22.32%), Space: 30.5 MB (71.99%) - LeetHub

---
+++ 3607-minimum-division-operations-to-make-array-non-decreasing/3607-minimum-division-operations-to-make-array-non-decreasing.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(len(nums)-2, -1, -1):
+            if nums[i] > nums[i+1]:
+                for j in range(2, int(sqrt(nums[i]))+1):
+                    if nums[i] % j == 0:
+                        nums[i] = j          
+                        break
+                if nums[i] > nums[i+1]:
+                    return -1
+                res += 1
+        print(nums)
+        return res
+            
