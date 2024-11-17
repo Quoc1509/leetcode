21f4176f06e0bdc7af5d5f86bf074ba73fef7ff8
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-16 23:45:49 -0800

    Time: 1 ms (11.09%), Space: 16.7 MB (8.77%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         N = len(nums)
-        memo = {}
-        def backTracking(i):
+        # memo = {}
+        # def backTracking(i):
+            
+        #     if i >= N: return 0
+        #     if i in memo: return memo[i]
+        #     res =  max(backTracking(i+1), backTracking(i+2)+nums[i])
+        #     memo[i] = res
+        #     return memo[i]
+        # return backTracking(0)
 
-            if i >= N: return 0
-            if i in memo: return memo[i]
-            res =  max(backTracking(i+1), backTracking(i+2)+nums[i])
-            memo[i] = res
-            return memo[i]
-        return backTracking(0)
+        # dp = [0] * (N+2)
+        # for i in range(N-1, -1, -1):
+        #     dp[i] = max(dp[i+1], dp[i+2]+nums[i])
+        # return dp[0]
+        @cache
+        def dfs(i, rob):
+            if i >= len(nums):
+                return 0
+            if rob:
+                return dfs(i+1, False)
+            else:
+                return max(dfs(i+1, False), dfs(i+1, True)+nums[i])
+        return dfs(0, False)
