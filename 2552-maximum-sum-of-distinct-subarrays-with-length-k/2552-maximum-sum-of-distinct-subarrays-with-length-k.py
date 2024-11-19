071d2b868ad9426bfcb942a2b7e17d4bf7ee8eb6
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-18 16:34:36 -0800

    Time: 129 ms (46.14%), Space: 36.4 MB (34.3%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -1,22 +1,18 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        res = 0
-        hashmap = defaultdict(int)
         total = 0
+        mp = defaultdict(int)
         for i in range(k):
-            hashmap[nums[i]] += 1
+            mp[nums[i]] += 1
             total += nums[i]
-        if len(hashmap) == k:
-            res = total
-        l = 0
-        for r in range(k, len(nums)):
-            hashmap[nums[r]] += 1
-            total += nums[r]
-            hashmap[nums[l]] -= 1
-            if hashmap[nums[l]] == 0:
-                del hashmap[nums[l]]
-            total -= nums[l]
-            l += 1
-            if len(hashmap) == k:
-                res = max(res, total)
+        res = total if len(mp) == k else 0
+        for i in range(k, len(nums)):
+            total -= nums[i-k]
+            mp[nums[i-k]] -= 1
+            if mp[nums[i-k]] == 0:
+                del mp[nums[i-k]]
+            total += nums[i]
+            mp[nums[i]] += 1
+            if len(mp) == k:
+                res = max(total, res)
         return res
