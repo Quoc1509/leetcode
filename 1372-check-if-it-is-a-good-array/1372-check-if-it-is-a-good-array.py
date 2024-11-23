c264b3a38dbccc99f69c91694ec45df1c85c11cb
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-22 16:06:40 -0800

    Time: 8 ms (50%), Space: 27.3 MB (77.48%) - LeetHub

--- 1372-check-if-it-is-a-good-array/1372-check-if-it-is-a-good-array.py
+++ 1372-check-if-it-is-a-good-array/1372-check-if-it-is-a-good-array.py
@@ -3,4 +3,4 @@ class Solution:
         res = nums[0]
         for i in range(1, len(nums)):
             res = gcd(res, nums[i])
-        return True if res == 1 else False
+        return res == 1
