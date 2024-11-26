d57691066ad34c944f282535e69a6757fd1e7c31
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-11-25 18:20:57 -0800

    Time: 184 ms (44.44%), Space: 31.5 MB (83.18%) - LeetHub

--- 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.py
+++ 3178-minimum-increment-operations-to-make-array-beautiful/3178-minimum-increment-operations-to-make-array-beautiful.py
@@ -1,14 +1,21 @@
 class Solution:
     def minIncrementOperations(self, nums: List[int], k: int) -> int:
-        @cache
-        def dp(i, j):
-            if i >= len(nums):
-                return 0
-            increase = inf
-            noIncrease = inf
-            if i - j < 3:
-                noIncrease = dp(i+1, j)
-            increase = dp(i+1, i) + max(0, k-nums[i])
-            return min(increase, noIncrease)
-        ans = dp(0, -1)
-        return ans
+        # @cache
+        # def dp(i, j):
+        #     if i >= len(nums):
+        #         return 0
+        #     increase = inf
+        #     noIncrease = inf
+        #     if i - j < 3:
+        #         noIncrease = dp(i+1, j)
+        #     increase = dp(i+1, i) + max(0, k-nums[i])
+        #     return min(increase, noIncrease)
+        # ans = dp(0, -1)
+
+        # return ans
+
+        dp = [0] * 3
+        for i in range(len(nums)):
+            temp = min(dp[(i-1)%3], dp[(i-2)%3], dp[(i-3)%3])
+            dp[i%3] = temp + max(0, k - nums[i])
+        return min(dp)
