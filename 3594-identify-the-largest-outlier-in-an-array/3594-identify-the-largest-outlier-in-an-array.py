127c8bfb7fac24304b3a0de0ec17759c33e3861f
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-04 00:29:52 -0800

    Time: 1911 ms (6.09%), Space: 29.6 MB (99.19%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
@@ -2,11 +2,12 @@ class Solution:
     def getLargestOutlier(self, nums: List[int]) -> int:
         count = Counter(nums)    
         total = sum(nums)
-        nums.sort(reverse=True)
+        res = -inf
         for num in nums:
             count[num] -= 1
             temp = total - num
             if temp/2 in count and count[temp/2] > 0:
-                return num
+                res = max(res, num)
             count[num] += 1
+        return res
             
