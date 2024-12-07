a06508b661dbc803b1cef1764438fbb5133ba0ce
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-06 18:54:33 -0800

    Time: 495 ms (92.11%), Space: 29.2 MB (30.91%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.py
@@ -1,16 +1,18 @@
 class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
-        l, r = 1, max(nums)
+        l , r = 1, max(nums)
+
         def check(mid):
             count = 0
             for num in nums:
-                count += ceil(num / mid)-1
-            return count > maxOperations
+                count += (num-1)//mid
+            return count <= maxOperations
+
         while l <= r:
             m = (l+r)//2
             if check(m):
-                l = m + 1
-            else:
                 r = m - 1
+            else:
+                l = m + 1
         return l
 
