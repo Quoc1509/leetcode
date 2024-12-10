34a800d7d0f9a2f4a2764d4f7c050f0a2c5fc312
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-09 16:52:17 -0800

    Time: 7 ms (90.54%), Space: 17.2 MB (17.44%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,26 +1,50 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        count = defaultdict(lambda:defaultdict(int))
-        i = 0
-        res = -1
+        # count = defaultdict(lambda:defaultdict(int))
+        # i = 0
+        # res = -1
+        # while i < len(s):
+        #     j = i+1
+        #     while j < len(s) and s[i] == s[j]:
+        #         j += 1
+        #     x = j-i
+            
+        #     if x > 2:
+        #         # there are 3 substrings if x > 2
+        #         res = max(res, x-2)
+        #     if x > 1:
+        #         # there are 2 substrings if x > 1
+        #         count[s[i]][x-1] += 2
+        #         if count[s[i]][x-1] >= 3:
+        #             res = max(res, x-1)
+        #     # there is 1 substring for every case
+        #     count[s[i]][x] += 1
+        #     if count[s[i]][x] >= 3:
+        #         res = max(res, x)
+        #     i = j
+        # return res     
+        
+
+# substring: "aaaaa.....a"
+#   + if len(substring) > 2: There are 3 substrings with length = len(substring)-2
+#   + if len(substring) > 1: There are 2 substrings with length = len(substring)-1
+#   + there is a substring with length = len(substring)
+        mp = defaultdict(int)
+        res, i = -1, 0
         while i < len(s):
             j = i+1
             while j < len(s) and s[i] == s[j]:
                 j += 1
             x = j-i
-            # print(s[i], x)
-            if x > 2:
-                res = max(res, x-2)
-            if x > 1:
-                count[s[i]][x-1] += 2
-                if count[s[i]][x-1] >= 3:
-                    res = max(res, x-1)
-            count[s[i]][x] += 1
-            if count[s[i]][x] >= 3:
-                res = max(res, x)
+            if j-i >= 3:
+                res = max(res, j-i-2)
+            mp[s[i:j]] += 1
             i = j
-
-        # print(count)
-        return res     
-        
-
+        for key, item in mp.items():
+            if item >= 3:
+                res = max(res, len(key))
+            elif item >= 2 and len(key) > 2:
+                res = max(res, len(key)-1)
+            elif key[:len(key)-1] in mp:
+                res = max(res, len(key)-1)
+        return res
