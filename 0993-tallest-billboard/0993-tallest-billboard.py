3a5ad60a7531e44b9faadda90a60dfb1d13a802f
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-20 17:52:18 -0800

    Time: 487 ms (61.54%), Space: 143.3 MB (28.57%) - LeetHub

--- 0993-tallest-billboard/0993-tallest-billboard.py
+++ 0993-tallest-billboard/0993-tallest-billboard.py
@@ -6,9 +6,12 @@ class Solution:
             if i >= len(rods):
                 return 0 if diff == 0 else -inf
             temp = -inf
-            return max(dfs(i+1, diff+rods[i])+rods[i], dfs(i+1, diff-rods[i]), dfs(i+1, diff))
-        
-        res = dfs(0, 0)
-        # print(res)
-        return res
+            if diff >= rods[i]:
+                temp = dfs(i+1, diff-rods[i])
+            else:
+                temp = dfs(i+1, abs(diff-rods[i])) + abs(diff-rods[i])
+            
+            return max(dfs(i+1, diff+rods[i])+rods[i], temp, dfs(i+1, diff))
+
+        return dfs(0, 0)
             
