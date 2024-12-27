1a0acba5651a7581c95daa25b79370d4b8313dd4
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-26 18:59:37 -0800

    Time: 63 ms (78.05%), Space: 23.3 MB (10.49%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.py
@@ -1,13 +1,19 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
         
-        @cache
-        def dfs(i, choose):
-            if i >= len(values):
-                return -inf
+        # @cache
+        # def dfs(i, choose):
+        #     if i >= len(values):
+        #         return -inf
             
-            if not choose:
-                return max(dfs(i+1, True) + (values[i] + i), dfs(i+1, False))
-            else:
-                return max(dfs(i+1, True), values[i]-i)
-        return dfs(0, False)
+        #     if not choose:
+        #         return max(dfs(i+1, True) + (values[i] + i), dfs(i+1, False))
+        #     else:
+        #         return max(dfs(i+1, True), values[i]-i)
+        # return dfs(0, False)
+        res = -inf
+        cur_max = 0
+        for i in range(len(values)):
+            res = max(res, cur_max+values[i]-i)
+            cur_max = max(cur_max, values[i]+i)
+        return res
