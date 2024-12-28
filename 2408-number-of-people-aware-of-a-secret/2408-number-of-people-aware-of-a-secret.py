a2a7813edf21e51fbac41b3856e5d0eaa4feecd3
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2024-12-27 19:08:15 -0800

    Time: 7 ms (68.53%), Space: 18.1 MB (11.19%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
@@ -1,11 +1,24 @@
 class Solution:
     def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
         N = 10**9+7
-        res = [0] * n
-        res[0] = 1
+        # res = [0] * n
+        # res[0] = 1
+        # for i in range(n):
+        #     index = i+delay
+        #     while index < i+forget and index < n:
+        #         res[index] += res[i]
+        #         index += 1
+        # return sum(res[n-forget:])%N
+        prefix = [0] * n
+        dp = [0] * n
+        dp[0] = 1
+
         for i in range(n):
-            index = i+delay
-            while index < i+forget and index < n:
-                res[index] += res[i]
-                index += 1
-        return sum(res[n-forget:])%N
+            if i >= forget:
+                dp[i] = prefix[i-delay]-prefix[i-forget]
+            elif i >= delay:
+                dp[i] = prefix[i-delay]
+            prefix[i] = (prefix[i-1] if i > 0 else 0) + dp[i]
+        print(prefix)
+        return (prefix[-1] - (prefix[-forget-1] if n-forget-1 >= 0 else 0))%N
+
