29b4b848a01ee386251d8ad657640c2932a051ba
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-01-05 00:00:59 -0800

    Time: 62 ms (57.14%), Space: 41.4 MB (30.09%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        N = 26*5*10**4
         pre = [0] * (len(s)+1)
         for i in range(len(shifts)):
             a,b,c = shifts[i]
@@ -10,12 +9,15 @@ class Solution:
             else:
                 pre[a] += 1
                 pre[b+1] += -1
-
-        for i in range(1, len(pre)):
+        
+        convert = (ord(s[0])-97+pre[0])%26
+        res = chr(97+convert)
+        for i in range(1, len(pre)-1):
             pre[i] += pre[i-1]
-
-        res = ''
-        for i in range(len(pre)-1):
-            convert = (ord(s[i])-97+pre[i]+N)%26
+            convert = (ord(s[i])-97+pre[i])%26
             res += chr(97+convert)
+        # res = ''
+        # for i in range(len(pre)-1):
+        #     convert = (ord(s[i])-97+pre[i])%26
+        #     res += 
         return res
