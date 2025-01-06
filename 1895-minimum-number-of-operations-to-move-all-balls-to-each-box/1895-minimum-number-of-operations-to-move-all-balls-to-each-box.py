abcc4af618d02922da3f1f299766a6fd7ec585c7
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-01-05 17:34:28 -0800

    Time: 11 ms (68.21%), Space: 18.1 MB (16.05%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,17 +1,18 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        surfix = [0] * (len(boxes)+1)
-        res = [0] * len(boxes)
-        one = 0
-        for i in range(len(boxes)-1, -1, -1):
-            surfix[i] = one + surfix[i+1] 
+        surfix = [0] * len(boxes)
+        one = 0 if boxes[-1] == '0' else 1
+        for i in range(len(surfix)-2, -1, -1):
+            surfix[i] += (surfix[i+1]+one)
             if boxes[i] == '1':
                 one += 1
-        one = 0
-        prefix = 0
-        for i in range(len(res)):
-            res[i] = prefix + surfix[i]
+        prefix = [0] * len(boxes)
+        one = 0 if boxes[0] == '0' else 1
+        for i in range(1, len(boxes)):
+            prefix[i] += (prefix[i-1]+one)
             if boxes[i] == '1':
                 one += 1
-            prefix += one
+        res = []
+        for i in range(len(boxes)):
+            res.append(prefix[i]+surfix[i])
         return res
