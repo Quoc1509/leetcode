e23b29c76c20ac5a905fe7e0740eff0c97a09ceb
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-01-16 17:53:05 -0800

    Time: 22 ms (32.05%), Space: 19.2 MB (30.39%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -4,13 +4,14 @@ class Solution:
         for i, e in enumerate(nums):
             res ^= (i + 1) ^ e
 
-        bit = res&(-res)
+        bit = res&(-res) # tim bit 1 dau tien cua mot so
         num = 0
         for i, e in enumerate(nums):
             if (i + 1) & bit:
                 num ^= (i + 1)
             if e & bit:
                 num ^= e
+
         for n in nums:
             if num == n:
                 return [num, res^num]
