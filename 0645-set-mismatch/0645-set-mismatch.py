17616282c40e8e44860f60ee1f3a031495d85713
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-01-16 17:31:28 -0800

    Time: 18 ms (45.07%), Space: 19 MB (48.45%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,11 +1,17 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        total, res, duplicate = 0, 0, 0
-        
+        res = 0
         for i, e in enumerate(nums):
             res ^= (i + 1) ^ e
-            if (total & (1 << e)) != 0:  
-                duplicate = e 
-            total |= (1 << e)  
 
-        return [duplicate, res ^ duplicate]
+        bit = res&(-res)
+        num = 0
+        for i, e in enumerate(nums):
+            if (i + 1) & bit:
+                num ^= (i + 1)
+            if e & bit:
+                num ^= e
+        for n in nums:
+            if num == n:
+                return [num, res^num]
+        return [res^num, num]
