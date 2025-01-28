a4a0b4f48197a3dcb8ef7751ee875e2cfb5cdf2c
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-01-27 19:44:39 -0800

    Time: 268 ms (34.76%), Space: 76.8 MB (9.72%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -1,20 +1,12 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         temp = sorted([a, b, c] for a, b, c in zip(startTime, endTime, profit))
-        def search(start, end):
-            l, r = start, len(temp)-1
-            while l <= r:
-                m = (l+r)//2
-                if temp[m][0] < end:
-                    l = m + 1
-                else:
-                    r = m - 1
-            return l
+        
         @cache
-        def dfs(i):                                                                                                     
+        def dfs(i):
             if i >= len(temp):
                 return 0  
-            index = search(i, temp[i][1])
+            index = bisect_left(temp, [temp[i][1], 0,0])
             res = dfs(index) + temp[i][-1] 
             res = max(res, dfs(i+1))
             return res
