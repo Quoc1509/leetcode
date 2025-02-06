967818beb44dcb78e394c00c266dc7ee36ac95da
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-05 17:29:20 -0800

    Time: 695 ms (44.64%), Space: 25.6 MB (36.65%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -0,0 +1,37 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        def conquer(arr1, arr2):
+            if not arr1:
+                return arr2
+            elif not arr2:
+                return arr1
+            i, j = 0, 0
+            temp = []
+            while i < len(arr1) and j < len(arr2):
+                if arr1[i] < arr2[j]:
+                    temp.append(arr1[i])
+                    i += 1
+                elif arr1[i] > arr2[j]:
+                    temp.append(arr2[j])
+                    j += 1
+                else:
+                    temp.append(arr1[i])
+                    temp.append(arr2[j])
+                    i += 1
+                    j += 1
+            temp.extend(arr1[i:])
+            temp.extend(arr2[j:])
+            return temp
+
+        def devide(arr):
+            if len(arr) <= 1:
+                return arr
+            N = len(arr)//2
+            l = devide(arr[:N])
+            r = devide(arr[N:])
+        
+            return conquer(l, r)
+        
+        res = devide(nums)
+        return res
