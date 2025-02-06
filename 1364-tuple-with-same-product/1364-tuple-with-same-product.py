1dc290df99b7418330a3645da6d05c2ec9b19705
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-05 17:10:18 -0800

    Time: 403 ms (41.23%), Space: 46.4 MB (65.4%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,12 +1,18 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         pre = defaultdict(int)
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1, len(nums)):
+        #         pre[nums[i]*nums[j]] += 1
+        # res = 0
+        # for val in pre.values():
+        #     while val > 1:
+        #         res += ((val-1) * (2**3))
+        #         val -= 1
+        # return res
+        res = 0
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
+                res += pre[nums[i]*nums[j]]
                 pre[nums[i]*nums[j]] += 1
-        res = 0
-        for val in pre.values():
-            while val > 1:
-                res += ((val-1) * (2**3))
-                val -= 1
-        return res
+        return res * 8
