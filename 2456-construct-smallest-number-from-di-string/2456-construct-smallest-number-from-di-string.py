a428b4d4ecd741e2a62ef5292c8b00ceb5439bd9
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-17 17:23:06 -0800

    Time: 0 ms (100%), Space: 17.7 MB (54.04%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.py
@@ -3,21 +3,19 @@ class Solution:
         res = [inf]
         def BT(i, val, check):
             if i > len(pattern):
-                res[0] = min(res[0], val)
-                return
+                res[0] = val
+                return True
             
             for j in range(1, 10):
                 if val == 0:
-                    BT(1, j, (1<<j)|check)
+                    if BT(1, j, (1<<j)|check):
+                        return True
                 if (1<<j)&check:
                     continue
                 
-                if pattern[i-1] == 'I':
-                    if j > val % 10:
-                        BT(i+1, val*10+j, check|(1<<j))
-                else:
-                    if j < val % 10:
-                        BT(i+1, val*10+j, check|(1<<j))
+                if (pattern[i-1] == 'I' and j > val % 10) or (pattern[i-1] == 'D' and j < val % 10):
+                        if BT(i+1, val*10+j, check|(1<<j)):
+                            return True
         
         BT(0, 0, 0)
         return str(res[0])
