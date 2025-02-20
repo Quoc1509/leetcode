059ebe90dc800fc80dfe728feca85c76567ace6c
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-19 17:12:28 -0800

    Time: 35 ms (61.83%), Space: 17.7 MB (82.42%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -1,12 +1,17 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        res = set()
-        def BT(s, check):
-            res.add(s)
-            for i in range(len(tiles)):
-                if (1<<i) & check:
+        s = sorted(tiles)
+        res = [0]  
+
+        def backtrack(used):
+            res[0] += 1 
+            for i in range(len(s)):
+                if (1 << i) & used: 
                     continue
-                BT(s+tiles[i], check|(1<<i))
-            
-        BT('', 0)
-        return len(res)-1
+                if i > 0 and s[i] == s[i - 1] and not ((1 << (i - 1)) & used):
+                    continue 
+                
+                backtrack(used | (1 << i)) 
+        
+        backtrack(0)
+        return res[0] - 1 
