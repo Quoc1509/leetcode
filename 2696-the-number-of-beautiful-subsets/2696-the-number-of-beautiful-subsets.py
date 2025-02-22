69bf8493172198d15f1736d88196e640f1845075
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-21 22:20:30 -0800

    Time: 1106 ms (79.68%), Space: 18 MB (44.35%) - LeetHub

---
+++ 2696-the-number-of-beautiful-subsets/2696-the-number-of-beautiful-subsets.py
@@ -0,0 +1,34 @@
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        # mp = [defaultdict(int) for _ in range(k)]
+        # for n in nums:
+        #     mp[n%k][n] += 1
+        # res = 1
+        # for m in mp:
+        #     dp0, dp1 = 1, 0
+        #     prev = -inf
+        #     for a in sorted(m):
+        #         v = pow(2, m[a]) - 1
+        #         ndp0 = (dp0 + dp1) * v
+        #         if a - prev == k:
+        #             ndp1 = dp0 * v
+        #         else:
+        #             ndp1 = (dp0 + dp1) * v
+        #         dp0, dp1 = ndp0, ndp1
+        #         prev = a
+        #     res *= dp0 + dp1
+        # return res - 1
+        res = [0]
+        visit = defaultdict(int)
+        def BT(idx, visit):
+            res[0] += 1
+            for i in range(idx, len(nums)):
+                if visit[nums[i]-k] > 0:
+                    continue
+                visit[nums[i]] += 1
+                BT(i+1, visit)
+                visit[nums[i]] -= 1
+                
+        BT(0, defaultdict(int))
+        return res[0]-1
