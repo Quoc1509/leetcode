0b39868949834f04e28763c7f793399904fffa8c
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-02-28 18:25:42 -0800

    Time: 43 ms (50.12%), Space: 26.1 MB (78.09%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.py
@@ -3,11 +3,13 @@ class Solution:
         # dp1[i]: maximum sum of subarray end at i without del one
         # dp2[i]: maximim sum of subarray end at i after del one
         
-        dp1 = {-1: -inf}
-        dp2 = {-1: -inf}
+        dp1 = -inf
+        dp2 = -inf
         res = -inf
-        for i in range(len(arr)):
-            dp1[i] = max(arr[i], dp1[i-1]+arr[i])
-            dp2[i] = max(dp1[i-1], dp2[i-1] + arr[i])
-            res = max(res, dp1[i], dp2[i])
+        for num in arr:
+            ndp1 = max(num, dp1+num)
+            ndp2 = max(dp1, dp2 + num)
+            res = max(res, ndp1, ndp2)
+            dp2 = ndp2
+            dp1 = ndp1
         return res
