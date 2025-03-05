6d180b219617ef43e5b557b1d067c8cd66fab7e0
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-04 18:11:44 -0800

    Time: 99 ms (23.69%), Space: 26 MB (8.28%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -1,12 +1,30 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        mp = defaultdict(int)
-        mp[0] = 1
-        total, res = 0, 0
+        # mp = defaultdict(int)
+        # mp[0] = 1
+        # total, res = 0, 0
 
-        for num in arr:
-            total += num
-            res += mp[1-(total%2)]
-            mp[total%2] += 1
+        # for num in arr:
+        #     total += num
+        #     res += mp[1-(total%2)]
+        #     mp[total%2] += 1
  
-        return res % (10**9+7)
+        # return res % (10**9+7)
+
+        N = 10**9+7
+        #dpOdd[i]: num of sub arrays have oddsum end at index i
+        #dpEven[i]: num of sub arrays have evensum end at index i
+        dpOdd = [0] * (len(arr)+1)
+        dpEven = [0] * (len(arr)+1)
+        for i in range(len(arr)):
+            if arr[i] % 2 == 0:
+                dpOdd[i+1] = dpOdd[i]
+                dpEven[i+1] = dpEven[i] + 1
+            else:
+                dpOdd[i+1] = dpEven[i] + 1
+                dpEven[i+1] = dpOdd[i]  
+        return sum(dpOdd)%N
+            
+
+
+
