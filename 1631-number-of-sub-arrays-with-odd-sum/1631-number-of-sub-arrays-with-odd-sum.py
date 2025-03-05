d6972c9cfd8ca9d26d8ea44204435f418e573bfb
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-04 18:18:18 -0800

    Time: 51 ms (88.02%), Space: 21.8 MB (91.52%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -14,16 +14,30 @@ class Solution:
         N = 10**9+7
         #dpOdd[i]: num of sub arrays have oddsum end at index i
         #dpEven[i]: num of sub arrays have evensum end at index i
-        dpOdd = [0] * (len(arr)+1)
-        dpEven = [0] * (len(arr)+1)
-        for i in range(len(arr)):
-            if arr[i] % 2 == 0:
-                dpOdd[i+1] = dpOdd[i]
-                dpEven[i+1] = dpEven[i] + 1
+
+        # dpOdd = [0] * (len(arr)+1)
+        # dpEven = [0] * (len(arr)+1)
+        # for i in range(len(arr)):
+        #     if arr[i] % 2 == 0:
+        #         dpOdd[i+1] = dpOdd[i]
+        #         dpEven[i+1] = dpEven[i] + 1
+        #     else:
+        #         dpOdd[i+1] = dpEven[i] + 1
+        #         dpEven[i+1] = dpOdd[i]  
+        # return sum(dpOdd)%N
+
+        dpOdd = 0
+        dpEven = 0
+        res = 0
+        for num in arr:
+            if num % 2 == 0:
+                dpEven += 1
             else:
-                dpOdd[i+1] = dpEven[i] + 1
-                dpEven[i+1] = dpOdd[i]  
-        return sum(dpOdd)%N
+                newdpOdd = dpEven + 1
+                dpEven = dpOdd
+                dpOdd = newdpOdd
+            res += dpOdd
+        return res % N
             
 
 
