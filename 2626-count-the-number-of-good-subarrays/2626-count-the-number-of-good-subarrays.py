8a4382487420adf9bf867622015a5c2f39ac3a88
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-04 23:50:12 -0800

    Time: 132 ms (54.03%), Space: 34.4 MB (81.54%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -1,16 +1,14 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
-        r, res, pair = 0, 0, 0
         count = defaultdict(int)
-        for l in range(len(nums)):
-            while r < len(nums) and pair < k:
-                count[nums[r]] += 1
-                pair += (count[nums[r]] - 1)
-                r += 1
-            if pair >= k:
-                res += (len(nums)-r+1)
-            # else: 
-            #     break
-            count[nums[l]] -= 1
-            pair -= count[nums[l]]
+        pair, res, l = 0, 0, 0
+        for r in range(len(nums)):
+            pair += count[nums[r]]
+            count[nums[r]] += 1
+            
+            while pair >= k:
+                res += len(nums)-r
+                count[nums[l]] -= 1
+                pair -= count[nums[l]]
+                l += 1
         return res
