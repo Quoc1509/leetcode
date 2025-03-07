e35137b0d2145a9263bcd45c5f400a3e8d60b4b2
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-06 18:16:04 -0800

    Time: 366 ms (62.19%), Space: 25.2 MB (63.13%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
@@ -6,19 +6,21 @@ for i in range(2, int((math.sqrt(N)) + 1)):
         for j in range(i*i, N+1, i):
             Prime[j] = False
 
-
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        res= [-1, -1]
-        temp, minNum = 0, inf
-        for k in range(left, right+1):
-            if Prime[k]:
-                if temp != 0:
-                    if k - temp < minNum:
-                        res = [temp, k]
-                        minNum = (k - temp)
-                temp = k
+        distance = inf
+        res = [-1, -1]
+        pre = -1
+        for i in range(left, right+1):
+            if Prime[i]:
+                print(i)
+                if pre == -1:
+                    pre = i
+                elif i - pre < distance:
+                    distance = i-pre
+                    res = [pre, i]
+                pre = i
+                
+                
         return res
-                        
-
                 
