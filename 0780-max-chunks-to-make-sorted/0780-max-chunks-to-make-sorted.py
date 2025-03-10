9127c84b6e6e7437c7c93a1e248033698e11adee
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-10 16:36:57 -0700

    Time: 0 ms (100%), Space: 17.8 MB (47.73%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.py
@@ -1,13 +1,21 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        res = 0
-        count = 0
-        total = 0
-        for i in range(len(arr)):
-            total += arr[i]
-            count += i
-            if count == total:
-                res += 1
-                count = 0
-                total = 0
-        return res
+        # res = 0
+        # count = 0
+        # total = 0
+        # for i in range(len(arr)):
+        #     total += arr[i]
+        #     count += i
+        #     if count == total:
+        #         res += 1
+        #         count = 0
+        #         total = 0
+        # return res
+
+        monoStack = []
+        for num in arr:
+            maxNum = num
+            while monoStack and monoStack[-1] > num:
+                maxNum = max(maxNum, monoStack.pop())
+            monoStack.append(maxNum) 
+        return len(monoStack)
