39b1df30dfe224875302cb7aa91b2eda7cfcbc77
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-10 23:42:39 -0700

    Time: 112 ms (44.71%), Space: 18.1 MB (17.65%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -4,9 +4,11 @@ class Solution:
         mp = defaultdict(int)
         for r in range(len(s)):
             mp[s[r]] += 1
-            while all(mp[c] > 0 for c in 'abc'):
+            while len(mp) == 3:
                 res += len(s)-r
                 mp[s[l]] -= 1
+                if mp[s[l]] == 0:
+                    del mp[s[l]]
                 l += 1
 
         return res
