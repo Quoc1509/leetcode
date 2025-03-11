1449f776c4373b33b52cbc82a2dab9a1c9160b66
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-10 22:48:58 -0700

    Time: 31 ms (30.82%), Space: 31.6 MB (5.31%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -2,17 +2,12 @@ class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         visit = set()
         res = set()
-        temp = deque()
-        for r in range(len(s)):
-            temp.append(s[r])
-            if len(temp) == 10:
-                st = ''.join(temp)
-                if st in visit:
-                    res.add(st)
-                else:
-                    visit.add(st)
-                temp.popleft()
-        
+        for i in range(len(s)-9):
+            temp = s[i:i+10]
+            if temp in visit:
+                res.add(temp)
+            else:
+                visit.add(temp)
         print(res, visit)
         return list(res)
         
