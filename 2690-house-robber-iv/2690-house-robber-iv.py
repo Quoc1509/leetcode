00fa4a406ede12c9bfde09b3eb0cf75961fdc8a0
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-14 20:09:17 -0700

    Time: 366 ms (41.8%), Space: 28.7 MB (50%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.py
+++ 2690-house-robber-iv/2690-house-robber-iv.py
@@ -1,22 +1,20 @@
 class Solution:
     def minCapability(self, nums: List[int], k: int) -> int:
-        l, r = 0, max(nums)
-
-        def check(m):
-            count, i = 0, 0
-
+        l, r = min(nums), max(nums)
+        def check(mid):
+            i, count = 0, 0
             while i < len(nums):
-                if nums[i] <= m:
+                if nums[i] <= mid:
                     count += 1
                     if count == k: return True
                     i += 1
-                i += 1
+                i += 1 
             return False
-
+                
         while l <= r:
-            mid = (l+r)//2
-            if check(mid):
-                r = mid-1
+            m = (l+r)//2
+            if check(m):
+                r = m - 1
             else:
-                l = mid+1
+                l = m + 1
         return l
