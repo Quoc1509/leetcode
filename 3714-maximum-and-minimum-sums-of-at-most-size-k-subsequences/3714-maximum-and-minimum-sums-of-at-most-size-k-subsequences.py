ab3096de23df4ddd8e8c78d6a6f2802f71fef132
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-17 17:46:08 -0700

    Time: 112 ms (99.02%), Space: 680.4 MB (5.01%) - LeetHub

--- 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences.py
+++ 3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences/3714-maximum-and-minimum-sums-of-at-most-size-k-subsequences.py
@@ -1,23 +1,29 @@
+
+dp = [[0] * (10**5) for _ in range(70)]
+for L in range(70):
+    pre = 0
+    for i in range(10**5):
+        if L == 0:
+            dp[L][i] = 1
+        else:
+            if i >= L:
+                dp[L][i] = pre
+                
+            pre += dp[L-1][i]
+
+for L in range(1, 70):
+    for i in range(10**5):
+
+        dp[L][i] += dp[L-1][i]
+
 class Solution:
     def minMaxSums(self, nums: List[int], k: int) -> int:
-        N = 10**9+7        
+        M = 10**9+7
+        N = len(nums)        
         nums.sort()
-        def CountSum(arr):
-            dp = [[0] * len(arr) for _ in range(k)]
-            res = 0
-            for L in range(k):
-                pre = 0
-                for i in range(len(arr)):
-                    if L == 0:
-                        dp[L][i] = 1
-                    else:
-                        if i >= L:
-                            dp[L][i] = pre
-                            
-                        pre += dp[L-1][i]
-                    res = (res + dp[L][i] * arr[i]) %N
-                    
-            return res % N
+        res = 0
+        for i in range(N):
+            res = (res + dp[k-1][i] * (nums[i] + nums[N-i-1])) % M    
 
-        return (CountSum(nums) + CountSum(nums[::-1]))%N
+        return res
         # print(dpMax)
