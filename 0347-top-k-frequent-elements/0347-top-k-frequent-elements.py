c93f5c10ef3fe51fab4e7aa7313d881137cc1d6f
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-19 15:38:28 -0700

    Time: 9 ms (29.43%), Space: 24.7 MB (7.5%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,11 +1,7 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
-        maxRange = 0
-        for item in count.values():
-            maxRange = max(maxRange, item)
-        
-        arr = [[] for _ in range(maxRange+1)] 
+        arr = [[] for _ in range(len(nums)+1)] 
         res = []
         for key, item in count.items():
             arr[item].append(key)
