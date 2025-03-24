2aa5a44766d9f8384cdb6c22653920a43bf9b61d
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-24 16:02:20 -0700

    Time: 23 ms (39.05%), Space: 25.4 MB (19.44%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -2,7 +2,7 @@ class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
         N = 10**9+7
         dist = [inf] * n
-        visit = [0] * n
+        # visit = [0] * n
         
         graph = defaultdict(list)
         for a, b, c in roads:
@@ -11,18 +11,27 @@ class Solution:
 
         heap = [(0, 0)]
         dist[0] = 0
-        visit[0] = 1
+        # visit[0] = 1
 
         while heap:
             d, nod = heappop(heap)
             if d > dist[nod]:
                 continue
             for dis, no in graph[nod]:
-                if d+dis == dist[no]:
-                    visit[no] = (visit[no] + visit[nod]) % N
+                # if d+dis == dist[no]:
+                #     visit[no] = (visit[no] + visit[nod]) % N
                 if d + dis < dist[no]:  
                     heappush(heap, (d+dis, no))
                     dist[no] = d+dis
-                    visit[no] = visit[nod]
-        return visit[-1]
+                    # visit[no] = visit[nod]
+        @cache
+        def dfs(node):
+            if node == n-1:
+                return 1
+            res = 0
+            for w, no in graph[node]:
+                if dist[node] + w == dist[no]:
+                    res += dfs(no)
+            return res%N
+        return dfs(0) 
 
