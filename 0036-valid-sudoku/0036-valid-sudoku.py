63064c161ac687467bd3486a7de2aa7296853a39
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 01:14:33 -0700

    Time: 4 ms (60.52%), Space: 17.8 MB (69.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,29 +1,24 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row = [set() for _ in range(9)]
-        col = [set() for _ in range(9)] 
-        count = 0
-        r, c = 0, 0
-        while count < 9:
-            square = set()
-            i, j = r, c
-            while i < r+3:
-                while j < c+3:
-                    num = board[i][j]
-                    if num in square or num in row[i] or num in col[j]:
-                        return False
-                    if num != '.':
-                        square.add(num)
-                        row[i].add(num)
-                        col[j].add(num)
-                    j += 1
-                i += 1
-                j = c
-            c += 3
-            if c == 9:
-                r += 3
-                c = 0
-            count += 1
-        return True
-                
+        col = [set() for _ in range(9)]
+        small = defaultdict(set)
 
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    if board[i][j] in row[i] or board[i][j] in col[j]:
+                        return False
+                                    
+                    box_index = (i // 3, j // 3)
+                    if board[i][j] in small[box_index]:
+                        return False
+
+                    row[i].add(board[i][j])
+                    col[j].add(board[i][j])
+                    small[box_index].add(board[i][j])
+
+        return True
