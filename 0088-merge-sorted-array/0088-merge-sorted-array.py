19260bb51626569c2098c087ed06ed114d4640c0
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 14:21:58 -0700

    Time: 0 ms (100%), Space: 17.8 MB (71.92%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,22 +3,44 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # i1 = m-1
+        # i2 = n-1
+        # i = len(nums1)-1
+        # while i1 >= 0 and i2 >= 0:
+        #     if nums1[i1] < nums2[i2]:
+        #         nums1[i] = nums2[i2]
+        #         i2 -= 1
+        #     else:
+        #         nums1[i] = nums1[i1]
+        #         i1 -= 1
+        #     i -= 1
+        # while i1 >= 0:
+        #     nums1[i] = nums1[i1]
+        #     i -= 1
+        #     i1 -= 1
+        # while i2 >= 0:
+        #     nums1[i] = nums2[i2]
+        #     i2 -= 1
+        #     i -=1
+
+
         i1 = m-1
         i2 = n-1
         i = len(nums1)-1
         while i1 >= 0 and i2 >= 0:
-            if nums1[i1] < nums2[i2]:
-                nums1[i] = nums2[i2]
-                i2 -= 1
-            else:
+            if nums1[i1] > nums2[i2]:
                 nums1[i] = nums1[i1]
                 i1 -= 1
+            else:
+                nums1[i] = nums2[i2]
+                i2 -= 1
             i -= 1
         while i1 >= 0:
             nums1[i] = nums1[i1]
-            i -= 1
             i1 -= 1
+            i -= 1
         while i2 >= 0:
             nums1[i] = nums2[i2]
             i2 -= 1
-            i -=1
+            i -= 1
+
