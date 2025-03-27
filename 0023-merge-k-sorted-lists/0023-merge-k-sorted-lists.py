374104ae67f4476cdf6777b8728a51940a1cd458
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-26 20:37:52 -0700

    Time: 9 ms (72.61%), Space: 20.3 MB (52.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,70 +5,24 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # heap = []
-        # for i in lists:
-        #     cur = i
-        #     while cur:
-        #         heappush(heap, (cur.val))
-        #         cur = cur.next
-        # head = None
-        # cur = head
-        # while heap:
-        #     b = heappop(heap)
-        #     if not head:     
-        #         head = ListNode(b, None)
-        #         cur = head
-        #     else:
-        #         cur.next = ListNode(b, None)
-        #         cur = cur.next
-        # return head
+        heap = []
 
-        def conquer(l1, l2):
-            if not l1:
-                return l2
-            if not l2:
-                return l1
-            cur1 = l1
-            cur2 = l2
-            begin = None
-            cur = begin
-            while cur1 and cur2:
-                if cur1.val < cur2.val:
-                    if not begin:
-                        begin = cur1
-                        cur = begin             
-                    else:
-                        cur.next = cur1
-                        cur = cur.next
-                    cur1 = cur1.next
-                else:
-                    if not begin:
-                        begin = cur2
-                        cur = begin
-                        
-                    else:
-                        cur.next = cur2
-                        cur = cur.next
-                    cur2 = cur2.next
+        mp = defaultdict(list)
+        for node in lists:
+            if not node:
+                continue
+            heappush(heap, node.val)
+            mp[node.val].append(node)
+        dummy = pre = ListNode(0, None)
+        while heap:
+            v = heappop(heap)
+            n = mp[v].pop()
+            pre.next = n
+            pre = pre.next
+            if n.next:
+                heappush(heap, (n.next.val))
+                mp[n.next.val].append(n.next)
+        return dummy.next
 
-            if cur1:
-                cur.next = cur1
-            if cur2:
-                cur.next = cur2
-            
-            return begin
-
-
-        def devide(link):
-            if not link: return None
-            if len(link) == 1:
-                return link[0]
-            half = len(link)//2
-            l1 = devide(link[:half])
-            l2 = devide(link[half:])
-            return conquer(l1, l2)
-        
-        head = devide(lists)
-        return head
 
             
