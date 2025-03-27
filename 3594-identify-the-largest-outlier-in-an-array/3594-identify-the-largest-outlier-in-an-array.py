66e621e3e36d45c1560a6565a7fec3c354ad6f9d
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 15:34:41 -0700

    Time: 426 ms (22.09%), Space: 28.5 MB (16.76%) - LeetHub

--- 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
+++ 3594-identify-the-largest-outlier-in-an-array/3594-identify-the-largest-outlier-in-an-array.py
@@ -4,9 +4,12 @@ class Solution:
         total = sum(nums)
         res = -inf
         for num in nums:
-            count[num] -= 1
+            
             temp = total - num
-            if temp/2 in count and count[temp/2] > 0:
+            if temp % 2 != 0:
+                continue
+            count[num] -= 1
+            if temp//2 in count and count[temp//2] > 0:
                 res = max(res, num)
             count[num] += 1
         return res
