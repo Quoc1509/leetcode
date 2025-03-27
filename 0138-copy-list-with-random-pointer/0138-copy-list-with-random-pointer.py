a62c03d42bcf479dfa267af318d466b1962016dc
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 16:17:40 -0700

    Time: 37 ms (82.64%), Space: 18.7 MB (20.08%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -12,24 +12,46 @@ class Solution:
         if not head:
             return None
 
-        mp = {}
-        cur = head
-        dummy = pre = Node(0, None, None)
-        while cur:
-            temp = Node(cur.val, None, None)
-            mp[cur] = temp
-            pre.next = temp
-            pre = pre.next
-            cur = cur.next
+        # mp = {}
+        # cur = head
+        # dummy = pre = Node(0, None, None)
+        # while cur:
+        #     temp = Node(cur.val, None, None)
+        #     mp[cur] = temp
+        #     pre.next = temp
+        #     pre = pre.next
+        #     cur = cur.next
 
-        cur1 = head
-        cur2 = dummy.next
-        while cur1:
-            if cur1.random:
-                cur2.random = mp[cur1.random]
-            cur1 = cur1.next
-            cur2 = cur2.next
-        return dummy.next
-            
+        # cur1 = head
+        # cur2 = dummy.next
+        # while cur1:
+        #     if cur1.random:
+        #         cur2.random = mp[cur1.random]
+        #     cur1 = cur1.next
+        #     cur2 = cur2.next
+        # return dummy.next
+
+        cur = head
+        while cur:
+            node = cur.next
+            temp = Node(cur.val, node, None)       
+            cur.next = temp
+            cur = node
+
+        cur = head
+        copy = cur.next
+        while cur:
+            if cur.random:
+                copy.random = cur.random.next
+            cur = copy.next
+            if cur:
+                copy = cur.next
+
+        cur = head.next
+        res = cur
+        while cur and cur.next:
+            cur.next = cur.next.next
+            cur = cur.next
+        return res
             
         
