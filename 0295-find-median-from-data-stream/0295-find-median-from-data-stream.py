69d1be7f67302d909e729dad3d9248a5515df9d5
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 21:43:16 -0700

    Time: 100 ms (96.33%), Space: 39.4 MB (76.78%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,34 +1,28 @@
-from sortedcontainers import SortedList
 class MedianFinder:
 
     def __init__(self):
-        self.heapMax = []
-        self.heapMin = []
+        self.maxHeap = []
+        self.minHeap = []
 
     def addNum(self, num: int) -> None:
-        if not self.heapMax and not self.heapMin:
-            heappush(self.heapMin, num)
-        elif self.heapMin and self.heapMin[0] <= num:
-            heappush(self.heapMin, num)
+        if not self.minHeap and not self.maxHeap:
+            heappush(self.minHeap, num)
+        elif self.minHeap and self.minHeap[0] <= num:
+            heappush(self.minHeap, num)
         else:
-            heappush(self.heapMax, -num)
-
-        if len(self.heapMin) > len(self.heapMax) + 1:
-            temp = heappop(self.heapMin)
-            heappush(self.heapMax, -temp)
-        elif len(self.heapMin) + 1 <= len(self.heapMax):
-            temp = heappop(self.heapMax)
-            heappush(self.heapMin, -temp)
-
+            heappush(self.maxHeap, -num)
+        
+        if len(self.minHeap) > len(self.maxHeap) + 1:
+            temp = heappop(self.minHeap)
+            heappush(self.maxHeap, -temp)
+        elif len(self.minHeap) + 1 <= len(self.maxHeap):
+            temp = heappop(self.maxHeap)
+            heappush(self.minHeap, -temp)
 
     def findMedian(self) -> float:
-        if (len(self.heapMax) + len(self.heapMin)) % 2 != 0:
-
-            return self.heapMin[0]
+        if (len(self.maxHeap) + len(self.minHeap)) % 2 != 0:
+            return self.minHeap[0]
         else:
-            return (self.heapMin[0] - self.heapMax[0])/2
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+            return (self.minHeap[0] - self.maxHeap[0])/ 2
+        
+        
