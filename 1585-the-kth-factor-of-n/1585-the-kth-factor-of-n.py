940597e4c98570c3ce850eae1c24f0e24a390d53
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-28 15:46:20 -0700

    Time: 0 ms (100%), Space: 18.1 MB (24%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
@@ -1,15 +1,17 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
         first = []
-        second = []
-        for i in range(1, int(sqrt(n))+1):
+        N = int(sqrt(n))
+        for i in range(1, N+1):
             if n % i == 0:
                 first.append(i)
-                if n//i != i:
-                    second.append(n//i)
-        print(first, second)
         if k <= len(first):
             return first[k-1]
-        elif k-len(first) <= len(second):
-            return second[len(second)-(k-len(first))]
-        return -1           
+        if N * N == n:
+            k += 1
+        if k > 2 * len(first):
+            return -1
+        k -= len(first)
+        k = len(first) - k
+        k += 1
+        return n // first[k-1]
