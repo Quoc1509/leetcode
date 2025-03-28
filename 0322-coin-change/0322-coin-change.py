455d3cb45769ab22b87f9e30f681d95e2be542e5
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 22:45:10 -0700

    Time: 815 ms (40.43%), Space: 38.4 MB (14.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,16 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # arr = [float('inf') for i in range(amount+1)]
+        # arr[0] = 0
         
-        memo = defaultdict(int)
-        def backTracking(total):
-            if total == 0: return 0
-            if total < 0: return inf
-            if total in memo: return memo[total]
-            count = inf
-            for i in coins:
-                temp = backTracking(total-i)
-                count = min(count, temp)
-            memo[total] = count + 1
-            return memo[total]
-        res = backTracking(amount)
-        return res if res != inf else -1 
+        # for coin in coins:
+        #     for j in range(coin, amount + 1):
+        #         arr[j] = min(arr[j], arr[j - coin] + 1)
+
+        # if arr[amount] != float('inf'):
+        #     return arr[amount]
+        # else: return -1
+
+        @cache
+        def DP(total):
+            if total == 0:
+                return 0
+            if total < 0:
+                return inf
+            res = inf
+            for num in coins:
+                res = min(res, DP(total-num))
+            return res + 1
+        res = DP(amount)
+        return res if res != inf else -1
+        
