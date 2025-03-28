897b6c876880bc8aabd5c61d47cae228d1f37c67
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-28 12:37:55 -0700

    Time: 175 ms (45.11%), Space: 25.4 MB (68.65%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -5,21 +5,21 @@ class Solution:
         temp = ['']
         res = []
         memo = {}
-        def DP(i):
+        def DP(i, w):
             if i in memo: return memo[i]
-            if i >= len(temp[0]):
+            if i >= len(w):
                 # print(temp[0])
                 return 0
             res = -inf
-            for j in range(i, len(temp[0])):
-                if temp[0][i:j+1] in listWord:
-                    res = max(res, DP(j+1)+1)
+            for j in range(i, len(w)):
+                if w[i:j+1] in listWord:
+                    res = max(res, DP(j+1, w)+1)
             memo[i] = res
             return memo[i]
 
         for word in words:
             memo = {}
-            temp[0] = word
-            if DP(0) > 1:
+            # temp[0] = word
+            if DP(0, word) > 1:
                 res.append(word)
         return res
