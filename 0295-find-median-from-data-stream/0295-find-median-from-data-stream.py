09e808167a9c0ccf6e292741b5c41a2e7973622e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-27 17:21:43 -0700

    Time: 139 ms (60.7%), Space: 39.4 MB (76.78%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -2,18 +2,31 @@ from sortedcontainers import SortedList
 class MedianFinder:
 
     def __init__(self):
-        self.sortList = SortedList()
+        self.heapMax = []
+        self.heapMin = []
 
     def addNum(self, num: int) -> None:
-        self.sortList.add(num)
+        if not self.heapMax and not self.heapMin:
+            heappush(self.heapMin, num)
+        elif self.heapMin and self.heapMin[0] <= num:
+            heappush(self.heapMin, num)
+        else:
+            heappush(self.heapMax, -num)
+
+        if len(self.heapMin) > len(self.heapMax) + 1:
+            temp = heappop(self.heapMin)
+            heappush(self.heapMax, -temp)
+        elif len(self.heapMin) + 1 <= len(self.heapMax):
+            temp = heappop(self.heapMax)
+            heappush(self.heapMin, -temp)
+
 
     def findMedian(self) -> float:
-        l = len(self.sortList)
-        if l %2 != 0:
-            return self.sortList[l//2]
-        else:
-            return (self.sortList[l//2] + self.sortList[(l//2)-1])/2
+        if (len(self.heapMax) + len(self.heapMin)) % 2 != 0:
 
+            return self.heapMin[0]
+        else:
+            return (self.heapMin[0] - self.heapMax[0])/2
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
