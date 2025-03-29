b0d3f8c203f763a16dc4786405165a26d9431854
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-28 19:30:26 -0700

    Time: 99 ms (26.56%), Space: 21.6 MB (56.5%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,43 +1,35 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        operate = []
-        nums = []
-        num = 0
-        def calculate(num1, num2, st):
-            if st == '+':
-                return (num2+num1)
-            elif st == '-':
-                return (num2-num1)
-            elif st == '*':
-                return (num2*num1)
+        stack = []
+        i = 0
+
+        def getNumber(i):
+            j = i + 1
+            while j < len(s) and s[j].isdigit():
+                j += 1
+            stack.append(int(s[i:j]))
+            return j
+        while i < len(s):
+            if s[i] == '+' or s[i] == ' ':
+                i += 1
+                continue
+
+            if s[i].isdigit() or s[i] == '-':
+                i = getNumber(i)
             else:
-                return (num2//num1)
-
-        for i in range(len(s)):
-            if s[i] == "+" or s[i] == '-' or s[i] == '*' or s[i] == '/':
-                if operate and operate[-1] == '*':
-                    temp = nums.pop()
-                    st = operate.pop()
-                    count = calculate(num, temp, st)
-                    nums.append(count)
-                    
-                elif operate and operate[-1] == '/':
-                    temp = nums.pop()
-                    st = operate.pop()
-                    count = calculate(num, temp, st)
-                    nums.append(count)
-                else:
-                    nums.append(num)
-                operate.append(s[i])
-                num = 0
-
-            elif s[i].isdigit():
-                num = num * 10 + int(s[i])
-        if operate and (operate[-1] == '*' or operate[-1] == '/'):
-            nums.append(calculate(num, nums.pop(), operate.pop()))
-        else:
-            nums.append(num)
-        res = nums[0]
-        for i in range(len(operate)):
-            res = calculate(nums[i+1], res, operate[i])
-        return res
+                
+                num1 = stack.pop()
+                j = getNumber(i+1)
+                num2 = stack.pop()
+                if s[i] == '*':
+                    stack.append(num1*num2)
+                elif s[i] == '/':
+                    temp1 = abs(num1)
+                    temp2 = abs(num2)
+                    num = temp1//temp2
+                    if num1*num2 < 0:
+                        num = -num
+                    stack.append(num)
+                i = j
+        return sum(stack)
+        
