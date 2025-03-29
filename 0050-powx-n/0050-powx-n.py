f5bbdfce2f33f8504ccde2445f234421424dc904
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-28 18:55:25 -0700

    Time: 0 ms (100%), Space: 17.9 MB (50.21%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,16 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        def dfs(n):
+            if n == 1:
+                return x
+            res = dfs(n//2)
+
+            return res * res if n % 2 == 0 else res*res*x
+        res = dfs(abs(n))
+        if n < 0:
+            return 1/res
+        return res
+    
+    
