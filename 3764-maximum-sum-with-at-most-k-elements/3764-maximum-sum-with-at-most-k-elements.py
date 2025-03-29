3b49d9c16fbbd1bfb6cc5226a994399a9f56f1b7
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-28 18:21:40 -0700

    Time: 409 ms (32.59%), Space: 46.8 MB (58.26%) - LeetHub

---
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]], limits: List[int], k: int) -> int:
+        res = []
+        M, N = len(grid), len(grid[0])
+        for i in range(M):
+            temp = 0
+            heap = []
+            for j in range(N):
+                heappush(heap, grid[i][j])
+                if len(heap) > limits[i]: 
+                    heappop(heap)
+            while heap:
+                heappush(res, heappop(heap))
+                if len(res) > k:
+                    heappop(res)
+        return sum(res)
+                
+            
