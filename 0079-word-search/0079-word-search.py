b5cb60bae8bd5b895e1b39824ceed0696c322bcf
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-30 18:14:33 -0700

    Time: 3770 ms (67.38%), Space: 17.9 MB (39.67%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         M, N = len(board), len(board[0])
-
         surround = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-        def dfs(r, c, ind):
-            
-            if ind == len(word): return True
-            if r < 0 or r >= M or c < 0 or c >= N or board[r][c] != word[ind]: return False
-            temp = board[r][c]
-            board[r][c] = '#'
-            for a, b in surround:
-                row, col = r+a, c+b      
-
-                if dfs(row, col, ind+1):
+        def BT(r, c, i):
+            if i >= len(word):
+                return True
+            if r < 0 or r >= M or c < 0 or c >= N or board[r][c] != word[i]:
+                return False
+            w = board[r][c]
+            board[r][c] = '.'
+            for x, y in surround:
+                ro, co = r+x, c+y
+                
+                if BT(ro, co, i+1):
                     return True
-            board[r][c] = temp
+
+            board[r][c] = w
+
             return False
-
-
-        for i in range(M):
-            for j in range(N):
-                if dfs(i, j, 0):
-                    
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if BT(r, c, 0):
                     return True
-
         return False
+            
+            
