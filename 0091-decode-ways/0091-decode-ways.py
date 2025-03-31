e332befdfcc3b061833cd19cc2a9a43e6810c102
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-31 15:52:36 -0700

    Time: 0 ms (100%), Space: 17.8 MB (51.46%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -6,11 +6,13 @@ class Solution:
         def dfs(index):
             if index == N:
                 return 1
+            if index > N:
+                return 0
             if index in memo: return memo[index]
             one, two = 0, 0
-            if index + 1 <= N and s[index] != "0":
+            if s[index] != "0":
                 one = dfs(index+1)
-            if index + 2 <= N and s[index] != "0" and int(s[index: index+2]) <= 26:
+            if s[index] != "0" and int(s[index: index+2]) <= 26:
                 two = dfs(index+2)
             memo[index] = (one + two)
             return memo[index]
