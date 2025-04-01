dec09a0d5d3fbf7ab35e71b5fa49c25038e7e21a
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-31 22:10:17 -0700

    Time: 1611 ms (15.7%), Space: 602.1 MB (5%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,35 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        N = len(prices)
-        # @cache
-        # def dfs(i, transaction, holding):
-        #     if i >= N: return 0
+        @cache
+        def dfs(i, holding, transaction):
+            if i >= len(prices):
+                return 0
 
-        #     if transaction == 0:
-        #         return dfs(i+1, 0, holding)
-        #     if holding:
-        #         sell = dfs(i+1, transaction-1, 0)+prices[i]
-        #         noSell = dfs(i+1, transaction, holding)
-        #         return max(sell, noSell)
-        #     else:
-        #         buy = dfs(i+1, transaction, 1) - prices[i]
-        #         noBuy = dfs(i+1, transaction, 0)
-        #         return max(buy, noBuy)
-        # return dfs(0, 2, 0)
-
-        dp = [[[-inf] * 2 for _ in range(3)] for _ in range(N+1)]
-        for i in range(3):
-            for j in range(2):
-                dp[N][i][j] = 0
-
-        for i in range(N-1, -1, -1):
-            for trans in range(3):
-                for holding in range(2):
-                    if trans == 0:
-                        dp[i][trans][holding] = dp[i+1][0][holding]
-                    else:
-                        if holding == 1:
-                            dp[i][trans][holding] = max(dp[i+1][trans-1][0] + prices[i], dp[i+1][trans][holding])
-                        else:
-                            dp[i][trans][holding] = max(dp[i+1][trans][1] - prices[i], dp[i+1][trans][0])
-        return dp[0][2][0]
+            if transaction == 0:
+                return dfs(i+1, holding, 0)
+            else:
+                if holding:
+                    return max(dfs(i+1, holding, transaction), dfs(i+1, False, transaction-1)+prices[i])
+                else:
+                    return max(dfs(i+1, holding, transaction), dfs(i+1, True, transaction)-prices[i])
+                
+        return dfs(0, False, 2)
