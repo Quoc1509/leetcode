c092685b07b35ecde32398bae3a4b7766c584932
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-31 19:37:19 -0700

    Time: 39 ms (76.67%), Space: 18.4 MB (12.1%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -7,33 +7,32 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        graph = defaultdict(list)
+        adj = defaultdict(list)
 
-        def dfs(cur, par):
-            if cur.left:
-                graph[cur.val].append(dfs(cur.left, cur.val))
-            if cur.right:
-                graph[cur.val].append(dfs(cur.right, cur.val))
-            if par != -1:
-                graph[cur.val].append(par)
-            return cur.val
-        dfs(root, -1)
+        def dfs(node, parent):
+            if parent:
+                adj[node.val].append(parent.val)
+            if node.left:
+                adj[node.val].append(node.left.val)
+                dfs(node.left, node)
+            if node.right:
+                adj[node.val].append(node.right.val)
+                dfs(node.right, node)
+            
+        dfs(root, None)     
 
-        res = []
         q = deque()
         q.append(target.val)
         visit = set()
         visit.add(target.val)
-        
-        while k >= 0:
+        while q and k > 0:
+            print(q)
             for _ in range(len(q)):
-                node = q.popleft()
-                if k == 0:
-                    res.append(node)
-                for n in graph[node]:
-                    if n not in visit:
-                        visit.add(n)
-                        q.append(n)
+                val = q.popleft()
+                for no in adj[val]:
+                    if no not in visit:
+                        q.append(no)
+                        visit.add(no)
             k -= 1
-
-        return res
+        return list(q)
+            
