e68e25d307177090f73982cb1838c3572c7dd604
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-03-31 22:12:28 -0700

    Time: 230 ms (22.66%), Space: 82.3 MB (11.13%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,19 +1,16 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        N = len(prices)
-
         @cache
-        def dfs(i, transaction, holding):
-            if i >= N: return 0
+        def dfs(i, holding, transaction):
+            if i >= len(prices):
+                return 0
 
-            if transaction == 0: 
-                return dfs(i+1, 0, holding)
-            if holding:
-                sell = dfs(i+1, transaction-1, 0) + prices[i]
-                noSell = dfs(i+1, transaction, holding)
-                return max(sell, noSell)
+            if transaction == 0:
+                return dfs(i+1, holding, 0)
             else:
-                buy = dfs(i+1, transaction, 1) - prices[i]
-                noBuy = dfs(i+1, transaction, 0)
-                return max(buy, noBuy)
-        return dfs(0, k, 0)
+                if holding:
+                    return max(dfs(i+1, holding, transaction), dfs(i+1, False, transaction-1)+prices[i])
+                else:
+                    return max(dfs(i+1, holding, transaction), dfs(i+1, True, transaction)-prices[i])
+                
+        return dfs(0, False, k)
