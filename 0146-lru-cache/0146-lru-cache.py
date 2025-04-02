fa65e865d65913f782fde8e8ed30c499189578f7
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-02 11:44:12 -0700

    Time: 116 ms (82.28%), Space: 78.6 MB (46.19%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,65 @@
+class LinkedList:
+    def __init__(self, key=0, value=0, pre_node=None, next_node=None):
+        self.key = key
+        self.value = value
+        self.prev = pre_node
+        self.next = next_node
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.mp = {}
+        self.capacity = capacity
+        self.head = LinkedList(0, 0, None, None)
+        self.tail = LinkedList(0, 0, None, None)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def add_node(self, node):
+        temp = self.head.next
+        self.head.next = node
+        node.prev = self.head
+        node.next = temp
+        temp.prev = node
+
+    def connect_node(self, node):
+        temp = node
+        node.prev.next = temp.next
+        node.next.prev = temp.prev
+
+    def get(self, key: int) -> int:   
+        if key not in self.mp:
+            return -1
+
+        node = self.mp[key]
+        self.connect_node(node)
+        self.add_node(node)
+
+        return self.mp[key].value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.mp:
+            node = self.mp[key]
+            node.value = value
+            self.connect_node(node)
+            self.add_node(node)
+            return
+
+        if len(self.mp) == self.capacity:
+            node = self.tail.prev
+            k = node.key
+            self.connect_node(node)
+            del self.mp[k]
+
+        node = LinkedList(key, value, None, None)
+        self.mp[key] = node
+        self.add_node(node)
+        
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
