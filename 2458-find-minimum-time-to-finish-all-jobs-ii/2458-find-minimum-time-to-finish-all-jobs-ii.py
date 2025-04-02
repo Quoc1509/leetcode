2ac323c0f491c7137c4e29e1e06da8adc7762b45
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-02 00:21:27 -0700

    Time: 110 ms (54.39%), Space: 33 MB (54.68%) - LeetHub

---
+++ 2458-find-minimum-time-to-finish-all-jobs-ii/2458-find-minimum-time-to-finish-all-jobs-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
+        jobs.sort()
+        workers.sort()
+        res = 0
+        for i in range(len(jobs)):
+            res = max(res, ceil(jobs[i]/workers[i]))
+        return res
