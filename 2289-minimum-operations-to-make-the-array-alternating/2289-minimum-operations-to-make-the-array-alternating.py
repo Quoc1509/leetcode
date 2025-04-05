db3a9361bf5f672f0c18a640ccf5dcf1074035b9
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-04 22:53:55 -0700

    Time: 314 ms (7.94%), Space: 40.1 MB (5.29%) - LeetHub

--- 2289-minimum-operations-to-make-the-array-alternating/2289-minimum-operations-to-make-the-array-alternating.py
+++ 2289-minimum-operations-to-make-the-array-alternating/2289-minimum-operations-to-make-the-array-alternating.py
@@ -12,9 +12,22 @@ class Solution:
         
         e = sorted([(item, key) for key, item in even.items()], reverse=True)
         o = sorted([(item, key) for key, item in odd.items()], reverse=True)
+
+        def helper(mp):
+            first, second = [0, 0], [0, 0]
+            for key, item in mp.items():
+                if item > first[0]:
+                    second[0], second[1] = first[0], first[1]
+                    first = [item, key]
+                elif item > second[0]:
+                    second = [item, key]
+            return [first, second]
+
         lene = len(nums)//2 + (1 if len(nums) % 2 == 1 else 0)
         leno = len(nums)//2 
-
+        e = helper(even)
+        o = helper(odd)
+        
         if e[0][1] != o[0][1]:
             return lene-e[0][0] + leno-o[0][0]
         else:
