f90771b48e360ecc07bbff6f81e801a5d79b8bee
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-04 20:26:56 -0700

    Time: 0 ms (100%), Space: 18.7 MB (10.39%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,22 +1,24 @@
 class Solution:
+
     def search(self, nums: List[int], target: int) -> bool:
+
         l, r = 0, len(nums)-1
-        while l <= r:
-            m = (l+r) // 2
-            if nums[m] == target: return True
-            if nums[m] < nums[r]:
-                if nums[m] < target <= nums[r]:
-                    l = m + 1
+        def helper(l, r):
+            while l <= r:
+                m = (l+r) // 2
+                if nums[m] == target: return True
+                if nums[m] < nums[r]:
+                    if nums[m] < target <= nums[r]:
+                        l = m + 1
+                    else:
+                        r = m - 1
+                elif nums[m] > nums[l]:
+                    if nums[l] <= target < nums[m]:
+                        r = m - 1
+                    else:
+                        l = m + 1
                 else:
-                    r = m - 1
-            elif nums[m] > nums[l]:
-                if nums[l] <= target < nums[m]:
-                    r = m - 1
-                else:
-                    l = m + 1
-            else:
-                if nums[l] == target or nums[r] == target: return True
-                r -= 1
-                l += 1
+                    return helper(l, m-1) or helper(m+1, r) 
             
-        return False
+            return False
+        return helper(l, r)
