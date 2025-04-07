638146029a841ba8fc92db06c78abcc9590408f1
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-07 15:44:03 -0700

    Time: 0 ms (100%), Space: 17.9 MB (34.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -26,9 +26,11 @@ class Solution:
             return nums[0]
         if len(nums) == 2:
             return max(nums)
-        dp = [0] * (N)
-        dp[0] = nums[0]
-        dp[1] = max(nums[1], nums[0])
+        
+        dp2 = nums[0]
+        dp1 = max(nums[1], nums[0])
         for i in range(2, N):
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i])
-        return dp[-1]
+            dp = max(dp1, dp2+nums[i])
+            dp2 = max(dp2, dp1)
+            dp1 = dp
+        return dp1
