6ed7f117f3b41a6b36c5992308708c0255e47932
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-11 16:53:12 -0700

    Time: 134 ms (85.32%), Space: 25.3 MB (67.04%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -1,25 +1,26 @@
 class Solution:
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        listWord = set(words)
-        # words.sort(key = lambda x: len(x), reverse = True)
-        temp = ['']
+        listWord = set()
+        words.sort(key = lambda x: len(x))
+        memp = {}
         res = []
-        memo = {}
-        def DP(i, w):
-            if i in memo: return memo[i]
-            if i >= len(w):
-                # print(temp[0])
-                return 0
-            res = -inf
-            for j in range(i, len(w)):
-                if w[i:j+1] in listWord:
-                    res = max(res, DP(j+1, w)+1)
-            memo[i] = res
+        def DP(i, word):
+            if i in memo:
+                return memo[i]
+            if i >= len(word):
+                return True
+            for j in range(i, len(word)):
+                if word[i: j+1] in listWord:
+                    if DP(j+1, word):
+                        memo[i] = True
+                        return memo[i]
+            memo[i] = False
             return memo[i]
+
 
         for word in words:
             memo = {}
-            # temp[0] = word
-            if DP(0, word) > 1:
+            if DP(0, word):
                 res.append(word)
+            listWord.add(word)
         return res
