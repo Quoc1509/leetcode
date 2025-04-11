8f0b6b18eb0c936006f21ad1255765cd04a19895
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-10 22:53:40 -0700

    Time: 36 ms (62.6%), Space: 20.3 MB (21.12%) - LeetHub

--- 1582-design-browser-history/1582-design-browser-history.py
+++ 1582-design-browser-history/1582-design-browser-history.py
@@ -1,4 +1,4 @@
-class Node:
+class Page:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
@@ -7,20 +7,20 @@ class BrowserHistory:
 
     def __init__(self, homepage: str):
         self.mp = {}    
-        self.head = Node(0, Node(homepage))
+        self.head = Page(0, Page(homepage))
         self.cur = 0
         self.mp[self.cur] = self.head
         self.last = 1
     
-    def addNode(self, cur, nextNode):
-        cur.next = nextNode
+    def addPage(self, cur, nextPage):
+        cur.next = nextPage
         self.mp[self.cur] = cur
 
     def visit(self, url: str) -> None:
-        newNode = Node(url)
-        curNode = self.mp[self.cur].next
+        newPage = Page(url)
+        curPage = self.mp[self.cur].next
         self.cur += 1
-        self.addNode(curNode, newNode)
+        self.addPage(curPage, newPage)
         self.last = self.cur + 1
 
     def back(self, steps: int) -> str:
