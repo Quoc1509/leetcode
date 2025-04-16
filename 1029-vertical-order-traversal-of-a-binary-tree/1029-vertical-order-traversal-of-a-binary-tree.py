8c4a3f70a243149191f270f48340ec45da734f93
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-16 00:12:01 -0700

    Time: 0 ms (100%), Space: 18.2 MB (18.29%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
@@ -8,20 +8,22 @@ class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         
         mp = defaultdict(list)
-
+        start, end = 0,  0
         def dfs(node, row, col):
             if not node:
                 return
+            nonlocal start, end
+            start = min(start, col)
+            end = max(end, col)
 
             mp[col].append((row, node.val))
             dfs(node.left, row + 1, col - 1)
             dfs(node.right, row + 1, col + 1)
         dfs(root, 0, 0)
         res = [[] for _ in range(len(mp))]
-        key = sorted(list(mp.keys()))
-        i = 0
 
-        for k in key:
+        i = 0
+        for k in range(start, end + 1):
             for a, b in sorted(mp[k]):
                 res[i].append(b)
             i += 1
