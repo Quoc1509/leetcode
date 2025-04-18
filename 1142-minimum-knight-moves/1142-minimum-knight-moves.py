18a5d96663fed2dc30207ecae359bfddbf3ac1d0
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-17 17:28:14 -0700

    Time: 110 ms (85.26%), Space: 19.1 MB (86.58%) - LeetHub

--- 1142-minimum-knight-moves/1142-minimum-knight-moves.py
+++ 1142-minimum-knight-moves/1142-minimum-knight-moves.py
@@ -1,22 +1,45 @@
+MOVES = (
+    (2, -1),
+    (2, 1),
+    (-2, 1),
+    (-2, -1),
+    (1, 2),
+    (1, -2),
+    (-1, 2),
+    (-1, -2),
+)
+
+def getDirection(a):
+    if a > 0:
+        return 1
+    elif a == 0:
+        return 0
+    else:
+        return -1
+
 class Solution:
-    def minKnightMoves(self, x: int, y: int) -> int:
-        move = [(-1, -2), (-2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2)]
-        res = 0
-        x = abs(x)
-        y = abs(y)
-        q = deque()
-        q.append([0, 0])
-        visit = set()
-        visit.add((0,  0))
-        while q:
-            for _ in range(len(q)):
-                r, c = q.popleft()
-                if r == x and c == y:
-                    return res
-                for ro, co in move:
-                    row, col = r+ro, c+co
-                    if -x <= row and -y <= col and (row, col) not in visit:
-                        q.append([row, col])
-                        visit.add((row, col))
-            res += 1
+    def minKnightMoves(self, tx: int, ty: int) -> int:
+        if (tx, ty) == (0, 0):
+            return 0
+
+        visited = set()
+        d = deque()
+        d.append((0, 0, 0))
+
+        while d:
+            loc = d.popleft()
+            x, y, move = loc
+            px, py = getDirection(tx-x), getDirection(ty-y)
+
+            for dx, dy in MOVES:
+                nx, ny = x+dx, y+dy
+                if (
+                    (abs(tx - x) > 7 and px * dx < 0)  or (abs(ty - y) > 7 and py * dy < 0) or
+                    (nx, ny) in visited
+                ):
+                    continue
+                if nx == tx and ny == ty:
+                    return move+1
+                visited.add((nx, ny))
+                d.append((nx, ny, move+1))
         return -1
