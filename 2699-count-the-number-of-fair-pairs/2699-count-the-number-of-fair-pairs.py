29dc98c0227d6d76c63dd9d68a403605f8ed43c3
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-18 18:19:33 -0700

    Time: 239 ms (46.46%), Space: 31.6 MB (19.86%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -2,26 +2,9 @@ class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
         res = 0
-        for i in range(len(nums)-1):
-            tmp = upper - nums[i]
-            # l, r = i+1, len(nums)-1
-            # while l <= r:
-            #     m = (l + r)//2
-            #     if tmp >= nums[m]:
-            #         l = m + 1
-            #     else: 
-            #         r = m - 1
-            # hi = r
-            # tmp = lower - nums[i]
-            # l, r = i + 1, len(nums)-1
-            # while l <= r:
-            #     m = (l+r)//2
-            #     if tmp <= nums[m]:
-            #         r = m - 1
-            #     else:
-            #         l = m + 1
-            r = bisect_right(nums, upper-nums[i], i+1, len(nums))
-            l = bisect_left(nums, lower-nums[i], i+1, len(nums))
-            res += (r-l)
-
+        for i in range(len(nums)):
+            left = bisect_left(nums, lower-nums[i], i+1, len(nums))
+            right = bisect_right(nums, upper-nums[i], i+1, len(nums))
+            
+            res += (right-left)
         return res
