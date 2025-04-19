e748690e3ed9924ee6741b7d6b5fa7300e8d4adc
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-18 18:07:32 -0700

    Time: 107 ms (18.94%), Space: 21.7 MB (44.84%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,22 +2,19 @@ class Solution:
     def calculate(self, s: str) -> int:
         stack = []
         i = 0
-
         def getNumber(i):
             j = i + 1
-            while j < len(s) and s[j].isdigit():
+            while j < len(s) and (s[j].isdigit() or s[j] == " "):
                 j += 1
-            stack.append(int(s[i:j]))
+            stack.append(int(s[i:j].strip()))
             return j
         while i < len(s):
             if s[i] == '+' or s[i] == ' ':
                 i += 1
                 continue
-
             if s[i].isdigit() or s[i] == '-':
                 i = getNumber(i)
             else:
-                
                 num1 = stack.pop()
                 j = getNumber(i+1)
                 num2 = stack.pop()
