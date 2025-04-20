54ebd69fcd3c8818d5cb5ee8abfef8ff92313269
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-20 00:26:50 -0700

    Time: 602 ms (10.5%), Space: 18.8 MB (99.61%) - LeetHub

--- 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.py
+++ 3812-smallest-palindromic-rearrangement-i/3812-smallest-palindromic-rearrangement-i.py
@@ -5,12 +5,12 @@ class Solution:
             arr[ord(c)-ord("a")] += 1
         first = ""
         second = "" 
-        extra = -1
+        extra = ""
         for i in range(26):
             while arr[i] > 1:
                 first += chr(i+97)
                 second += chr(i+97)
                 arr[i] -= 2
             if arr[i] == 1:
-                extra = i
-        return first + (chr(extra+97) if extra != -1 else "") + second[::-1]
+                extra = chr(i+97)
+        return first + extra + second[::-1]
