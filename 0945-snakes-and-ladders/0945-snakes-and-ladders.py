e298dc512c45b90b644163ee69c52dd06d9fff4b
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-21 18:52:54 -0700

    Time: 17 ms (76.14%), Space: 18.1 MB (37.52%) - LeetHub

---
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -0,0 +1,43 @@
+class Solution:
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        M, N = len(board), len(board[0])
+        move = defaultdict(int)
+        
+        count = 1
+        direct = 0
+        for i in range(M-1, -1, -1):
+            
+            if direct % 2 == 0:
+                for j in range(N):
+                    if board[i][j] != -1:
+                        move[count] = board[i][j]
+                    count += 1
+            else:
+                for j in range(N-1, -1, -1):
+                    if board[i][j] != -1:
+                        move[count] = board[i][j]
+                    count += 1
+            direct += 1
+        # print(move)
+        
+        res = 0
+        q = deque()
+        q.append(1)
+        visit = set()
+        visit.add(1)
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur ==  M*N:
+                    return res                   
+                for i in range(1, 7):
+                    new_cur = cur + i  
+                    if new_cur in move:
+                        new_cur = move[new_cur]
+                    if new_cur not in visit:
+                        q.append(new_cur)
+                        visit.add(new_cur)
+            res += 1
+        return -1 
+
+                
