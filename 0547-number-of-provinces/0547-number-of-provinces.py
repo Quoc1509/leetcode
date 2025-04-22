68ccce09e357b4d9486b141d61af36d1dd143211
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-21 23:30:16 -0700

    Time: 14 ms (18.14%), Space: 18.9 MB (68.96%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,21 +1,35 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                if isConnected[i][j] == 1:
-                    graph[i].append(j)
-        visited = set()
+        N = len(isConnected)
+        rank = [0]*N
 
-        def dfs(city):
-            visited.add(city)
-            for j in graph[city]:
-                if j not in visited:
-                   temp = dfs(j)
+        father = [i for i in range(N)] 
+        def root(x):
+            if x == father[x]:
+                return x
+            return root(father[x])
 
-        res = 0
-        for i in graph:
-           if i not in visited:
-            dfs(i)
-            res += 1
-        return res
+        def union(a, b):
+            a = root(a)
+            b = root(b)
+            if a == b:
+                return
+            if rank[a] < rank[b]:
+                father[a] = b
+            elif rank[a] > rank[b]:
+                father[b] = a
+            else:
+                father[b] = a
+                rank[a] += 1
+        
+        for i in range(N):
+            for j in range(N):
+                if isConnected[i][j]: 
+                    union(i, j)
+        res = set()
+        for i in range(N):
+            res.add(root(i))
+        print(father)
+        return len(res)
+    
+        
