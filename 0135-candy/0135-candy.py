f9a0fee3bb1c9f123f608d78e0414e788b9e8ef1
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-23 15:59:24 -0700

    Time: 15 ms (69.2%), Space: 20 MB (36.86%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,13 +5,9 @@ class Solution:
         for i in range(1, N):
             if arr[i] > arr[i-1]:
                 left[i] = left[i-1] + 1
-                
-        right = [1] * N
-        right[-1] = left[-1]
+        
         for i in range(N-2, -1, -1):
             if arr[i] > arr[i+1]:
-                right[i] = max(right[i+1]+1, left[i])
+                left[i] = max(left[i+1]+1, left[i])
         res = 0
-        for i in range(N):
-            res += max(left[i], right[i])
-        return res
+        return sum(left)
