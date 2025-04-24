fc89ece0b6a91ae01768d6464c1d7a9bd0d33187
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-04-24 15:02:04 -0700

    Time: 55 ms (97.23%), Space: 18 MB (29.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -16,20 +16,36 @@ class Solution:
 
         # return s[left:right+1]
 
-        def palindromic(start, end):
+        # def palindromic(start, end):
             
-            while start >= 0 and end < len(s) and s[start] == s[end]:
-                start -= 1
-                end += 1
-            return start+1, end
+        #     while start >= 0 and end < len(s) and s[start] == s[end]:
+        #         start -= 1
+        #         end += 1
+        #     return start+1, end
 
-        l, r = 0, 0
-        for i in range(len(s)):
-            st, en = palindromic(i, i)
-            if en-st > r-l:
-                l, r = st, en
-            st, en = palindromic(i, i+1)
-            if en-st > r-l:
-                l, r = st, en
-        return s[l:r]
+        # l, r = 0, 0
+        # for i in range(len(s)):
+        #     st, en = palindromic(i, i)
+        #     if en-st > r-l:
+        #         l, r = st, en
+        #     st, en = palindromic(i, i+1)
+        #     if en-st > r-l:
+        #         l, r = st, en
+        # return s[l:r]
+        ss = "$"
+        for c in s:
+            ss += "#" + c
+        ss += "#^"
+        dp = [0] * len(ss)
+        L, R = 1, 1
+        for i in range(1, len(ss)-1):
+            dp[i] = max(0, min(dp[R-i+L], R-i))
+            while ss[i+dp[i]] == ss[i-dp[i]]:
+                dp[i] += 1
+            if i + dp[i] > R:
+                L = i-dp[i]
+                R = i+dp[i]
+        ma = max(dp)
+        center = dp.index(ma)
+        return s[(center-ma)//2:((center+ma)//2)-1]
             
