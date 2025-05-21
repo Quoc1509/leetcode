0d45fba0df378092cdeaa21fa26784b5e0109052
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-05-20 21:55:47 -0700

    Time: 3 ms (73.14%), Space: 18.7 MB (9.48%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,29 +4,51 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         M, N = len(matrix), len(matrix[0])
-        for i in range(M):
-            if 0 in matrix[i]:
-                for j in range(N):
-                    if matrix[i][j] == 0:
-                        continue
-                    matrix[i][j] = -inf
+        # for i in range(M):
+        #     if 0 in matrix[i]:
+        #         for j in range(N):
+        #             if matrix[i][j] == 0:
+        #                 continue
+        #             matrix[i][j] = -inf
                  
-        for i in range(N):
-            check = 0
-            for j in range(M):   
-                if matrix[j][i] == 0:
-                    check = 1
-                    break
-            if check:
-                for j in range(M):
-                    if matrix[j][i] == 0:
-                        continue
-                    matrix[j][i] = -inf
+        # for i in range(N):
+        #     check = 0
+        #     for j in range(M):   
+        #         if matrix[j][i] == 0:
+        #             check = 1
+        #             break
+        #     if check:
+        #         for j in range(M):
+        #             if matrix[j][i] == 0:
+        #                 continue
+        #             matrix[j][i] = -inf
+
+        # for i in range(M):
+        #     for j in range(N):
+        #         if matrix[i][j] == 0:
+        #             continue
+        #         if matrix[i][j] == -inf:
+        #             matrix[i][j] = 0
+        zero = []
+        def setX(x):
+            for i in range(N):
+                matrix[x][i] = 0
+        def setY(y):
+            for i in range(M):
+                matrix[i][y] = 0
 
         for i in range(M):
             for j in range(N):
                 if matrix[i][j] == 0:
-                    continue
-                if matrix[i][j] == -inf:
-                    matrix[i][j] = 0
+                    zero.append((i,j))
+        visitX = set()
+        visitY = set()
+        while zero:
+            x, y = zero.pop()
+            if x not in visitX:
+                setX(x)
+                visitX.add(x)
+            if y not in visitY:
+                setY(y)
+                visitY.add(y)
         
