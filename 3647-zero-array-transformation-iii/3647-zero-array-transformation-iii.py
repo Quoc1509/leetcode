c8fa944db9455faf1a90177fa2efe57ed431991e
Quoc1509 <66648660+Quoc1509@users.noreply.github.com>
2025-05-22 11:27:45 -0700

    Time: 391 ms (26.2%), Space: 63 MB (5.24%) - LeetHub

---
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        queries.sort(key=lambda x: (x[0], -x[1]))
+        heap = []
+        q = 0
+        diff = [0] * (len(nums)+1)
+        for i in range(len(nums)):
+            while q < len(queries) and queries[q][0] == i:
+                heappush(heap, -queries[q][1])
+                q += 1
+            while nums[i] > diff[i]:
+                if not heap or -heap[0] < i:
+                    return -1
+                end = -heappop(heap)
+                diff[i] += 1
+                diff[end+1] -= 1
+            diff[i+1] += diff[i]
+        return len(heap)
+            
